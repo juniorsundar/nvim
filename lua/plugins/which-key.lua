60fcc1dd2d540a6e21ee850ac8647c308b0b2472
Junior Sundar <juniorsundar@gmail.com>
2024-07-03 23:43:05 +0400

    Squashing a lot of history

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,193 +1,117 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 500
-	end,
-	config = function()
-		local keymap = vim.keymap -- for conciseness
-		local status_ok, which_key = pcall(require, "which-key")
-		if not status_ok then
-			return
-		end
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 500
+    end,
+    opts = {
+        preset = "modern",
+        icons = {
+            rules = false,
+        },
+        win = {
+            row = -1,
+            -- border = "rounded",
+            padding = { 1, 2 }, -- extra window padding [top/bottom, right/left]
+            title = true,
+            title_pos = "center",
+            zindex = 1000,
+            -- Additional vim.wo and vim.bo options
+            bo = {},
+            wo = {
+                -- winblend = 10, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+            },
+        },
+        layout = {
+            width = { min = 20 }, -- min and max width of the columns
+            spacing = 3,          -- spacing between columns
+            align = "center",     -- align columns left, center or right
+        },
+        spec = {
+            { "<leader>a",         "<cmd>Alpha<cr>",                                                              desc = "Alpha" },
+            { "<leader>w",         "<cmd>w!<cr>",                                                                 desc = "Save" },
+            { "<leader>q",         "<cmd>q<cr>",                                                                  desc = "Quit" },
+            { "<leader>l",         "<cmd>Lazy<cr>",                                                               desc = "Lazy" },
+            { "<leader>m",         "<cmd>Mason<cr>",                                                              desc = "Mason" },
+            { "<leader>o",         "<cmd>Oil<cr>",                                                                desc = "Oil" },
+            { "<leader>t",         "<cmd>terminal<cr>",                                                           desc = "Terminal" },
+            { "<leader>u",         "<cmd>UndotreeToggle<cr>",                                                     desc = "Undotree" },
+            { "<leader>c",         "<cmd>bdelete<cr>",                                                            desc = "Close Buffer" },
+            -- { "<leader>b",         "<cmd>FzfLua buffers<cr>",                                                     desc = "Buffers" },
+            { "<leader>b",         "<cmd>Telescope buffers layout_strategy=vertical<cr>",                         desc = "Buffers" },
+            { "<leader>/",         "<cmd>Grepper -tool rg -query<cr>",                                            desc = "Grepper" },
 
-		local setup = {
-			plugins = {
-				marks = true, -- shows a list of your marks on ' and `
-				registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-				spelling = {
-					enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-					suggestions = 20, -- how many suggestions should be shown in the list?
-				},
-				presets = {
-					operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-					motions = true, -- adds help for motions
-					text_objects = true, -- help for text objects triggered after entering an operator
-					windows = true, -- default bindings on <c-w>
-					nav = true, -- misc bindings to work with windows
-					z = true, -- bindings for folds, spelling and others prefixed with z
-					g = true, -- bindings for prefixed with g
-				},
-			},
-			key_labels = {
-				-- Custom labels for keys
-			},
-			icons = {
-				breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-				separator = "➜", -- symbol used between a key and its label
-				group = "+", -- symbol prepended to a group
-			},
-			popup_mappings = {
-				scroll_down = "<c-d>", -- binding to scroll down inside the popup
-				scroll_up = "<c-u>", -- binding to scroll up inside the popup
-			},
-			window = {
-				border = "rounded", -- none, single, double, shadow
-				position = "bottom", -- bottom, top
-				margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-				padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-				winblend = 0,
-			},
-			layout = {
-				height = { min = 4, max = 25 }, -- min and max height of the columns
-				width = { min = 20, max = 50 }, -- min and max width of the columns
-				spacing = 3, -- spacing between columns
-				align = "center", -- align columns left, center or right
-			},
-			ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-			hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-			show_help = true, -- show help message on the command line when the popup is visible
-			triggers = "auto", -- automatically setup triggers
-			triggers_blacklist = {
-				i = { "j", "k" },
-				v = { "j", "k" },
-			},
-		}
+            { "<leader>L",         group = "LSP" },
+            { "<leader>LD",        group = "Document" },
+            { "<leader>LW",        group = "Workspace" },
+            { "<leader>La",        "<cmd>Lspsaga code_action<cr>",                                                desc = "Code Action" },
+            { "<leader>Lf",        "<cmd>lua vim.lsp.buf.format{async=true}<cr>",                                 desc = "Format" },
+            { "<leader>Ll",        "<cmd>lua vim.lsp.codelens.run()<cr>",                                         desc = "CodeLens Action" },
+            { "<leader>Ln",        "<cmd>Lspsaga rename<cr>",                                                     desc = "Rename" },
+            { "<leader>Lr",        "<cmd>Lspsaga finder ref ++normal<cr>",                                        desc = "References" },
+            { "<leader>Lk",        "<cmd>Lspsaga hover_doc<cr>",                                                  desc = "Hover" },
+            { "<leader>Lt",        "<cmd>Lspsaga finder tyd ++normal<cr>",                                        desc = "Type Definition" },
+            { "<leader>Lh",        "<cmd>lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())<cr>", desc = "Type Hint" },
+            { "<leader>Lo",        "<cmd>Lspsaga outline<cr>",                                                    desc = "Outline" },
+            { "<leader>LI",        "<cmd>LspInfo<cr>",                                                            desc = "LSP Info" },
+            { "<leader>LDd",       "<cmd>Lspsaga show_buf_diagnostics ++normal<cr>",                              desc = "Document Diagnostics" },
+            -- { "<leader>LDs",       "<cmd>FzfLua lsp_document_symbols<cr>",                                        desc = "Document Symbols" },
+            { "<leader>LDs",       "<cmd>Telescope lsp_document_symbols layout_strategy=vertical<cr>",            desc = "Document Symbols" },
+            { "<leader>LDj",       "<cmd>Lspsaga diagnostic_jump_next<CR>",                                       desc = "Next Diagnostic" },
+            { "<leader>LWa",       "<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>",                             desc = "Add Workspace Folder" },
+            { "<leader>LWd",       "<cmd>Lspsaga show_workspace_diagnostics ++normal<cr>",                        desc = "Workspace Diagnostics" },
+            -- { "<leader>LWs",       "<cmd>FzfLua lsp_workspace_symbols<cr>",                                       desc = "Workspace Symbols" },
+            { "<leader>LWs",       "<cmd>Telescope lsp_workspace_symbols layout_strategy=vertical<cr>",           desc = "Workspace Symbols" },
+            { "<leader>LWr",       "<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>",                          desc = "Remove Workspace Folder" },
+            { "<leader>LWl",       "<cmd>lua vim.lsp.buf.list_workspace_folders()<cr>",                           desc = "List Workspace Folders" },
 
-		local opts = {
-			mode = "n", -- NORMAL mode
-			prefix = "<leader>",
-			buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-			silent = true, -- use `silent` when creating keymaps
-			noremap = true, -- use `noremap` when creating keymaps
-			nowait = true, -- use `nowait` when creating keymaps
-		}
+            { "<leader>F",         desc = "Find", },
+            -- { "<leader>Ff",       "<cmd>FzfLua files<cr>",                                                       desc = "Files" },
+            -- { "<leader>Ft",       "<cmd>FzfLua live_grep<CR>",                                                   desc = "Text" },
+            -- { "<leader>Fc",       "<cmd>FzfLua colorschemes<cr>",                                                desc = "Colorscheme" },
+            -- { "<leader>Fh",       "<cmd>FzfLua helptags<cr>",                                                    desc = "Find Help" },
+            -- { "<leader>Fk",       "<cmd>FzfLua keymaps<cr>",                                                     desc = "Keymaps" },
+            -- { "<leader>Fr",       "<cmd>FzfLua oldfiles<cr>",                                                    desc = "Open Recent File" },
+            -- { "<leader>FT",       "<cmd>FzfLua tabs<cr>",                                                        desc = "Tabs" },
+            -- { "<leader>FM",       "<cmd>FzfLua manpages<cr>",                                                    desc = "Man Pages" },
+            -- { "<leader>FR",       "<cmd>FzfLua registers<cr>",                                                   desc = "Registers" },
+            -- { "<leader>FC",       "<cmd>FzfLua commands<cr>",                                                    desc = "Commands" },
+            { "<leader>Ff",        "<cmd>Telescope find_files layout_strategy=vertical<cr>",                      desc = "Files" },
+            { "<leader>Ft",        "<cmd>Telescope live_grep layout_strategy=vertical<CR>",                       desc = "Text" },
+            { "<leader>Fc",        "<cmd>Telescope colorscheme layout_strategy=vertical<cr>",                     desc = "Colorscheme" },
+            { "<leader>Fh",        "<cmd>Telescope help_tags layout_strategy=vertical<cr>",                       desc = "Find Help" },
+            { "<leader>Fk",        "<cmd>Telescope keymaps layout_strategy=vertical<cr>",                         desc = "Keymaps" },
+            { "<leader>Fr",        "<cmd>Telescope oldfiles layout_strategy=vertical<cr>",                        desc = "Open Recent File" },
+            { "<leader>FM",        "<cmd>Telescope man_pages layout_strategy=vertical<cr>",                       desc = "Man Pages" },
+            { "<leader>FR",        "<cmd>Telescope registers layout_strategy=vertical<cr>",                       desc = "Registers" },
+            { "<leader>FC",        "<cmd>Telescope commands layout_strategy=vertical<cr>",                        desc = "Commands" },
 
-		local function list_workspace_folders()
-			local folders = vim.lsp.buf.list_workspace_folders()
-			if not folders or vim.tbl_isempty(folders) then
-				print("No workspace folders found")
-				return
-			end
 
-			local qf_list = {}
-			for _, folder in ipairs(folders) do
-				table.insert(qf_list, { filename = folder, lnum = 1, col = 1, text = folder })
-			end
+            { "<leader>A",         group = "Autocompletion", },
+            { "<leader>Ae",        "<cmd>lua require 'cmp'.setup{ enabled = true }<cr>",                          desc = "Enabled" },
+            { "<leader>Ad",        "<cmd>lua require 'cmp'.setup{ enabled  false }<cr>",                          desc = "Disabled" },
 
-			vim.fn.setqflist(qf_list, "r")
-			vim.cmd("copen")
-		end
+            { "<leader>G",         group = "Git", },
+            -- { "<leader>Go",        "<cmd>FzfLua git_status<cr>",                                                  desc = "Open changed file" },
+            { "<leader>Go",        "<cmd>Telescope git_status layout_strategy=vertical<cr>",                      desc = "Open changed file" },
+            -- { "<leader>Gb",        "<cmd>FzfLua git_branches<cr>",                                                desc = "Checkout branch" },
+            { "<leader>Gb",        "<cmd>Telescope git_branches layout_strategy=vertical<cr>",                    desc = "Checkout branch" },
+            { "<leader>Gd",        "<cmd>Gitsigns diffthis HEAD<cr>",                                             desc = "Diff" },
 
-		local mappings = {
-			a = { "<cmd>Alpha<cr>", "Alpha" },
-			w = { "<cmd>w!<cr>", "Save" },
-			q = { "<cmd>q<cr>", "Quit" },
-			l = { "<cmd>Lazy<cr>", "Lazy" },
-			m = { "<cmd>Mason<cr>", "Mason" },
-			o = { "<cmd>Oil<cr>", "Oil" },
-			t = { "<cmd>terminal<cr>", "Terminal" },
-			u = { "<cmd>UndotreeToggle<cr>", "Undotree" },
-			c = { "<cmd>bdelete<cr>", "Close Buffer" },
-			b = { "<cmd>FzfLua buffers<cr>", "Buffers" },
-            ["/"] = { "<cmd>GrugFar<cr>", "GrugFar" },
-			-- Autocompletion
-			A = {
-				name = "Autocompletion",
-				e = { "<cmd>lua require 'cmp'.setup{ enabled = true }<cr>", "Enabled" },
-				d = { "<cmd>lua require 'cmp'.setup{ enabled = false }<cr>", "Disabled" },
-			},
-			-- Find
-			F = {
-				name = "Find",
-				f = { "<cmd>FzfLua files<cr>", "Files" },
-				t = { "<cmd>FzfLua live_grep<CR>", "Text" },
-				c = { "<cmd>FzfLua colorschemes<cr>", "Colorscheme" },
-				h = { "<cmd>FzfLua helptags<cr>", "Find Help" },
-				k = { "<cmd>FzfLua keymaps<cr>", "Keymaps" },
-				r = { "<cmd>FzfLua oldfiles<cr>", "Open Recent File" },
-				T = { "<cmd>FzfLua tabs<cr>", "Tabs" },
-				M = { "<cmd>FzfLua manpages<cr>", "Man Pages" },
-				R = { "<cmd>FzfLua registers<cr>", "Registers" },
-				C = { "<cmd>FzfLua commands<cr>", "Commands" },
-			},
-			-- Git
-			G = {
-				name = "Git",
-				o = { "<cmd>FzfLua git_status<cr>", "Open changed file" },
-				b = { "<cmd>FzfLua git_branches<cr>", "Checkout branch" },
-				d = { "<cmd>Gitsigns diffthis HEAD<cr>", "Diff" },
-			},
-			-- Language Server Protocol (LSP)
-			L = {
-				name = "LSP",
-				-- a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-				a = { "<cmd>Lspsaga code_action<cr>", "Code Action" },
-				f = { "<cmd>lua vim.lsp.buf.format{async=true}<cr>", "Format" },
-				l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-				n = { "<cmd>Lspsaga rename<cr>", "Rename" },
-				-- r = { "<cmd>lua vim.lsp.buf.references()<cr>", "References" },
-				r = { "<cmd>Lspsaga finder ref ++normal<cr>", "References" },
-				-- d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Definition" },
-				-- c = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "Declaration" },
-				-- i = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "Implementation" },
-				-- k = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Hover" },
-				k = { "<cmd>Lspsaga hover_doc<cr>", "Hover" },
-				-- t = { "<cmd>lua vim.lsp.buf.goto_type_definition<cr>", "Type Definition" },
-				t = { "<cmd>Lspsaga finder tyd ++normal<cr>", "Type Definition" },
-				h = { "<cmd>lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())<cr>", "Type Hint" },
-				o = { "<cmd>Lspsaga outline<cr>", "Outline" },
-				I = { "<cmd>LspInfo<cr>", "LSP Info" },
-				D = {
-					name = "Document",
-					-- d = { "<cmd>FzfLua diagnostics_document<cr>", "Document Diagnostics" },
-					d = { "<cmd>Lspsaga show_buf_diagnostics ++normal<cr>", "Document Diagnostics" },
-					s = { "<cmd>FzfLua lsp_document_symbols<cr>", "Document Symbols" },
-					-- j = { "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", "Next Diagnostic" },
-					j = { "<cmd>Lspsaga diagnostic_jump_next<CR>", "Next Diagnostic" },
-					-- k = { "<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>", "Prev Diagnostic" },
-					k = { "<cmd>Lspsaga diagnostic_jump_prev<cr>", "Prev Diagnostic" },
-				},
-				W = {
-					name = "Workspace",
-					a = { "<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>", "Add Workspace Folder" },
-					-- d = { "<cmd>FzfLua diagnostics_workspace<cr>", "Workspace Diagnostics" },
-					d = { "<cmd>Lspsaga show_workspace_diagnostics ++normal<cr>", "Workspace Diagnostics" },
-					s = { "<cmd>FzfLua lsp_workspace_symbols<cr>", "Workspace Symbols" },
-					r = { "<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>", "Remove Workspace Folder" },
-					l = { list_workspace_folders, "List Workspace Folders" },
-				},
-			},
+            { "<leader>N",         group = "Neorg", },
+            { "<leader>NJ",        group = "Journal", },
+            { "<leader>NM",        group = "Metadata", },
+            { "<leader>Ni",        "<cmd>Neorg index<cr>",                                                        desc = "Index" },
+            { "<leader>NJt",       "<cmd>Neorg journal today<cr>",                                                desc = "Today's Journal" },
+            { "<leader>NJm",       "<cmd>Neorg journal tomorrow<cr>",                                             desc = "Tomorrow's Journal" },
+            { "<leader>NJy",       "<cmd>Neorg journal yesterday<cr>",                                            desc = "Yesterday's Journal" },
+            { "<leader>NMi",       "<cmd>Neorg inject-metadata<cr>",                                              desc = "Inject" },
+            { "<leader>NMu",       "<cmd>Neorg update-metadata<cr>",                                              desc = "Update" },
 
-			N = {
-				name = "Neorg",
-				i = { "<cmd>Neorg index<cr>", "Index" },
-				J = {
-					name = "Journal",
-					t = { "<cmd>Neorg journal today<cr>", "Today's Journal" },
-					m = { "<cmd>Neorg journal tomorrow<cr>", "Tomorrow's Journal" },
-					y = { "<cmd>Neorg journal yesterday<cr>", "Yesterday's Journal" },
-				},
-				M = {
-					name = "Metadata",
-					i = { "<cmd>Neorg inject-metadata<cr>", "Inject" },
-					u = { "<cmd>Neorg update-metadata<cr>", "Update" },
-				},
-			},
-		}
-
-		which_key.setup(setup)
-		which_key.register(mappings, opts)
-	end,
+            { "<leader><leader>",  group = "LocalLeader", },
+            { "<leader><leader>n", group = "org-roam", },
+            { "<leader><leader>o", group = "org-mode", },
+        },
+    },
 }
