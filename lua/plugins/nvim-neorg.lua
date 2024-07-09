5ebd8518309ee1ee06ff06ebc09d94ad977ee18b
Junior Sundar <juniorsundar@gmail.com>
2024-07-09 22:33:52 +0400

    Updated lockfile

--- lua/plugins/nvim-neorg.lua
+++ lua/plugins/nvim-neorg.lua
@@ -25,7 +25,7 @@ return {
                     },
                     ["core.concealer"] = {
                         config = {
-                            -- icon_preset = "diamond",
+                            icon_preset = "diamond",
                             icons = { list = { icons = { "󰧞", "", "", "", "", "" } } },
                         },
                     },
