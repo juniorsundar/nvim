b4176a01d2265bf7fff4f5eaa020218c5f58e2d6
Junior Sundar <junior.sundar@tii.ae>
2024-07-17 21:39:22 +0400

    Update lazy lock
    update lazy lock
    
    Updated lazulock'lazy-lock

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,45 +2,80 @@ return {
     {
         "catppuccin/nvim",
         name = "catppuccin",
-        priority = 1000,
         config = function()
-            require("catppuccin").setup({
+            local cyberdream = {
+                color_overrides = {
+                    macchiato = {
+                        base = "#16181A",
+                        blue = "#5EA1FF",
+                        crust = "#3C4048",
+                        flamingo = "#FF9F9F",
+                        green = "#5EFF6C",
+                        lavender = "#FF5EF1",
+                        mantle = "#1E2124",
+                        maroon = "#D96666",
+                        mauve = "#BD5EFF",
+                        overlay0 = "#474A55",
+                        overlay1 = "#5A5E6B",
+                        overlay2 = "#6E7280",
+                        peach = "#FFBD5E",
+                        pink = "#FF5EA0",
+                        red = "#FF6E5E",
+                        rosewater = "#FFD1DC",
+                        sapphire = "#4A90E2",
+                        sky = "#5EF1FF",
+                        subtext0 = "#8A8E99",
+                        subtext1 = "#A0A4B8",
+                        surface0 = "#26282E",
+                        surface1 = "#2E3138",
+                        surface2 = "#363940",
+                        teal = "#64D8CB",
+                        text = "#FFFFFF",
+                        yellow = "#F1FF5E",
+                    },
+                },
                 custom_highlights = function(colors)
-                    return {
-                        WhichKeyFloat = { fg = colors.base, bg = colors.base },
-                    }
+                    return {}
                 end,
+                highlight_overrides = {
+                    macchiato = function(colors)
+                        return {
+                            NormalFloat = { fg = colors.text, bg = colors.base },
+                            Comment = { fg = colors.overlay1 },
+                            LineNr = { fg = colors.overlay1 },
+                            CursorLineNr = { fg = colors.text },
+                            WhichKey = { fg = colors.text, bg = colors.base },
+                            WhichKeyNormal = { fg = colors.text, bg = colors.base },
+                            Folded = { fg = colors.crust, bg = colors.none },
+                        }
+                    end,
+                },
+                transparent_background = false,
                 integrations = {
                     cmp = true,
                     gitsigns = true,
-                    treesitter = true,
-                    alpha = true,
-                    barbar = true,
-                    flash = true,
+                    treesitter = false,
                     markdown = true,
                     neogit = true,
+                    snacks = true,
+                    blink_cmp = true,
                     treesitter_context = true,
                     lsp_saga = true,
-                    lsp_trouble = true,
                     diffview = true,
                     which_key = true,
                     mason = true,
-                    noice = true,
                     notify = true,
                     native_lsp = {
                         enabled = true,
                     },
                     mini = true,
+                    nvim_surround = true,
                 },
-            })
-            vim.cmd.colorscheme("catppuccin-frappe")
+            }
+
+            require("catppuccin").setup(cyberdream)
+            vim.cmd.colorscheme "catppuccin-macchiato"
         end,
-    },
-    {
-        "nyoom-engineering/oxocarbon.nvim"
-    },
-    {
-        "folke/tokyonight.nvim",
     },
     {
         "nvim-tree/nvim-web-devicons",
