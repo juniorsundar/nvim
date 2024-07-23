6020ced9dc6d1cf084d336366fb27da546c807e3
Junior Sundar <juniorsundar@gmail.com>
2024-07-24 00:33:08 +0400

    Updated norg latex server

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,6 +18,7 @@ return {
                     "gopls",
                     "markdown_oxide",
                     "marksman",
+                    "ltex"
                 },
             })
         end,
@@ -76,7 +77,7 @@ return {
                 },
             }
 
-            local lspconfig =require("lspconfig")
+            local lspconfig = require("lspconfig")
 
             local function lspSymbol(name, icon)
                 local hl = "DiagnosticSign" .. name
@@ -185,6 +186,41 @@ return {
 
             lspconfig.marksman.setup({
                 single_file_support = false,
+            })
+
+            lspconfig.ltex.setup({
+                capabilities = capabilities,
+                filetypes = { "norg" },
+                settings = {
+                    ltex = {
+                        enabled = { "norg" },
+                        language = "en-GB",
+                        checkFrequency = "save",
+                    },
+                    dictionary = (function()
+                        -- For dictionary, search for files in the runtime to have
+                        -- and include them as externals the format for them is
+                        -- dict/{LANG}.txt
+                        --
+                        -- Also add dict/default.txt to all of them
+                        local files = {}
+                        for _, file in ipairs(vim.api.nvim_get_runtime_file("dict/*", true)) do
+                            local lang = vim.fn.fnamemodify(file, ":t:r")
+                            local fullpath = vim.fs.normalize(file, ":p")
+                            files[lang] = { ":" .. fullpath }
+                        end
+
+                        if files.default then
+                            for lang, _ in pairs(files) do
+                                if lang ~= "default" then
+                                    vim.list_extend(files[lang], files.default)
+                                end
+                            end
+                            files.default = nil
+                        end
+                        return files
+                    end)(),
+                }
             })
 
             -- Use LspAttach autocommand to only map the following keys
