9c4ca9187caac97c2e668e6487ff0cf89668c3e1
Junior Sundar <juniorsundar@gmail.com>
2024-08-21 23:43:42 +0400

    Updated fold level in ufo

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -29,9 +29,8 @@ return {
             dependencies = { 'kevinhwang91/promise-async' },
             config = function()
                 vim.o.foldenable = true
-                vim.o.foldlevel = 99
+                -- vim.o.foldlevel = 99
                 vim.o.foldlevelstart = 99
-                vim.o.foldcolumn = '0'
                 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
                 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
