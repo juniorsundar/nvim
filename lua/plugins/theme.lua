8d98128b3418d3d61cb468013a26fbe8315eba10
Junior Sundar <junior.sundar@tii.ae>
2024-10-22 10:07:12 +0400

    Updated theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,6 +3,75 @@ return {
         "catppuccin/nvim",
         name = "catppuccin",
         config = function()
+            local modus_vivendi = {
+                color_overrides = {
+                    macchiato = {
+                        rosewater = "#fae3e0",     -- Light red/rosewater equivalent
+                        flamingo = "#f8c7c3",      -- Flamingo equivalent
+                        pink = "#d7a3ff",          -- Bright pink
+                        mauve = "#a899ff",         -- Mauve equivalent
+                        red = "#ff8059",           -- Bright red
+                        maroon = "#ff6a6a",        -- Maroon equivalent
+                        peach = "#ffa94d",         -- Bright peach/orange
+                        yellow = "#e5c07b",        -- Yellow equivalent
+                        green = "#44bc44",         -- Bright green
+                        teal = "#00bc7f",          -- Teal equivalent
+                        sky = "#00f5e9",           -- Sky/cyan equivalent
+                        sapphire = "#1fa3cf",      -- Sapphire equivalent
+                        blue = "#4f97d7",          -- Bright blue
+                        lavender = "#d7afff",      -- Light purple
+                        text = "#ffffff",          -- Foreground (bright text)
+                        subtext1 = "#dcdcdc",      -- Subtext1 equivalent
+                        subtext0 = "#bcbcbc",      -- Subtext0 equivalent
+                        overlay2 = "#9c9c9c",      -- Overlay2 equivalent
+                        overlay1 = "#8c8c8c",      -- Overlay1 equivalent
+                        overlay0 = "#7c7c7c",      -- Overlay0 equivalent
+                        surface2 = "#5b6078",      -- Surface2 equivalent
+                        surface1 = "#494d64",      -- Surface1 equivalent
+                        surface0 = "#363a4f",      -- Surface0 equivalent
+                        base = "#000000",          -- Background
+                        mantle = "#121212",        -- Darker background
+                        crust = "#1e1e1e",         -- Dark crust (similar to bg_highlight)
+                    },
+                },
+                custom_highlights = function(colors)
+                    return {
+                    }
+                end,
+                highlight_overrides = {
+                    macchiato = function(colors)
+                        return {
+                            NormalFloat = { fg = colors.text, bg = colors.mantle },
+                            Comment = { fg = colors.overlay1 },
+                            LineNr = { fg = colors.overlay1 },
+                            CursorLineNr = { fg = colors.text },
+                            WhichKey = { fg = colors.text, bg = colors.base },
+                            WhichKeyNormal = { fg = colors.text, bg = colors.base },
+                            Folded = { fg = colors.crust, bg = colors.none },
+                            ["@neorg.tags.ranged_verbatim.code_block"] = { bg = colors.crust }
+                        }
+                    end,
+                },
+                transparent_background = false,
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    treesitter = false,
+                    -- markdown = true,
+                    neogit = true,
+                    treesitter_context = true,
+                    lsp_saga = true,
+                    diffview = true,
+                    which_key = true,
+                    mason = true,
+                    notify = true,
+                    native_lsp = {
+                        enabled = true,
+                    },
+                    mini = true,
+                    nvim_surround = true
+                },
+            }
             local cyberdream = {
                 color_overrides = {
                     macchiato = {
@@ -56,19 +125,13 @@ return {
                     cmp = true,
                     gitsigns = true,
                     treesitter = false,
-                    alpha = true,
-                    barbar = true,
-                    flash = true,
                     -- markdown = true,
                     neogit = true,
-                    grug_far = true,
                     treesitter_context = true,
                     lsp_saga = true,
-                    lsp_trouble = true,
                     diffview = true,
                     which_key = true,
                     mason = true,
-                    noice = true,
                     notify = true,
                     native_lsp = {
                         enabled = true,
@@ -78,8 +141,8 @@ return {
                 },
             }
 
-            require("catppuccin").setup(cyberdream)
-            -- require("catppuccin").setup(modus_vivendi)
+            -- require("catppuccin").setup(cyberdream)
+            require("catppuccin").setup(modus_vivendi)
             vim.cmd.colorscheme("catppuccin-macchiato")
         end,
     },
