98061809d2ac38441e57cd4143418549c0f6792c
Junior Sundar <juniorsundar@gmail.com>
2024-11-15 22:30:25 +0400

    Refactor

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,20 +1,18 @@
 return {
-    {
-        "ibhagwan/fzf-lua",
-        -- optional for icon support
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            -- calling `setup` is optional for customization
-            local actions = require("fzf-lua.actions")
-            require("fzf-lua").setup({
-                winopts = {
-                    split = "belowright new",
-                    preview = {
-                        horizontal = "right:50%",
-                        delay = 50
-                    }
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        -- calling `setup` is optional for customization
+        local actions = require("fzf-lua.actions")
+        require("fzf-lua").setup({
+            winopts = {
+                split = "belowright new",
+                preview = {
+                    horizontal = "right:50%",
+                    delay = 50
                 }
-            })
-        end
-    }
+            }
+        })
+    end
 }
--- lua/plugins/nvim-neorg.lua
+++ lua/plugins/nvim-neorg.lua
@@ -1,113 +1,111 @@
 return {
-    {
-        "nvim-neorg/neorg",
-        event = "VeryLazy",
-        dependencies = { {
-            dir = "~/.config/nvim_plugins/neorg-extras",
-            -- {
-            --     "juniorsundar/neorg-extras",
-            --     tag = "v0.5.0"
-            -- }
-        },
-        },
-        config = function()
-            require("neorg").setup({
-                load = {
-                    ["core.defaults"] = {},
-                    ["core.esupports.indent"] = {},
-                    ["core.concealer"] = {
-                        config = {
-                            icon_preset = "varied",
-                            icons = {
-                                list = {
-                                    icons = { "󰧞", "", "", "", "", "" }
-                                },
-                                heading = {
-                                    icons = { "󰼏", "󰼐", "󰼑", "󰼒", "󰼓", "󰼔" },
-                                },
+    "nvim-neorg/neorg",
+    event = "VeryLazy",
+    dependencies = { {
+        dir = "~/.config/nvim_plugins/neorg-extras",
+        -- {
+        --     "juniorsundar/neorg-extras",
+        --     tag = "v0.5.0"
+        -- }
+    },
+    },
+    config = function()
+        require("neorg").setup({
+            load = {
+                ["core.defaults"] = {},
+                ["core.esupports.indent"] = {},
+                ["core.concealer"] = {
+                    config = {
+                        icon_preset = "varied",
+                        icons = {
+                            list = {
+                                icons = { "󰧞", "", "", "", "", "" }
+                            },
+                            heading = {
+                                icons = { "󰼏", "󰼐", "󰼑", "󰼒", "󰼓", "󰼔" },
                             },
                         },
-                    },
-                    ["core.dirman"] = {
-                        config = {
-                            workspaces = {
-                                default = "~/Dropbox/neorg/notes",
-                                the_good_teacher = "~/Dropbox/neorg/tgt",
-                                god_of_war = "~/Dropbox/neorg/gow",
-                            },
-                        },
-                    },
-                    ["core.export"] = {},
-                    ["core.journal"] = {
-                        config = {
-                            strategy = "flat"
-                        }
-                    },
-                    ["core.highlights"] = {},
-                    ["core.export.markdown"] = {},
-                    ["core.completion"] = {
-                        config = {
-                            engine = "nvim-cmp",
-                        }
-                    },
-                    ["core.latex.renderer"] = {},
-                    ["core.integrations.image"] = {},
-                    ["core.integrations.nvim-cmp"] = {},
-                    ["core.summary"] = {},
-                    ["core.qol.toc"] = {},
-                    ["core.qol.todo_items"] = {},
-                    ["core.looking-glass"] = {},
-                    ["core.presenter"] = {
-                        config = {
-                            zen_mode = "zen-mode",
-                        }
-                    },
-                    ["core.tangle"] = {
-                        config = {
-                            report_on_empty = false,
-                        }
-                    },
-                    ["core.tempus"] = {},
-                    ["core.ui.calendar"] = {},
-                    ["external.agenda"] = {},
-                    ["external.roam"] = {
-                        config = {
-                            fuzzy_finder = "Fzf",
-                            fuzzy_backlinks = false,
-                            roam_base_directory = "vault"
-                        }
-                    },
-                    ["external.many-mans"] = {
-                        config = {
-                            metadata_fold = true,
-                            code_fold = false,
-                        }
                     },
                 },
-            })
+                ["core.dirman"] = {
+                    config = {
+                        workspaces = {
+                            default = "~/Dropbox/neorg/notes",
+                            the_good_teacher = "~/Dropbox/neorg/tgt",
+                            god_of_war = "~/Dropbox/neorg/gow",
+                        },
+                    },
+                },
+                ["core.export"] = {},
+                ["core.journal"] = {
+                    config = {
+                        strategy = "flat"
+                    }
+                },
+                ["core.highlights"] = {},
+                ["core.export.markdown"] = {},
+                ["core.completion"] = {
+                    config = {
+                        engine = "nvim-cmp",
+                    }
+                },
+                ["core.latex.renderer"] = {},
+                ["core.integrations.image"] = {},
+                ["core.integrations.nvim-cmp"] = {},
+                ["core.summary"] = {},
+                ["core.qol.toc"] = {},
+                ["core.qol.todo_items"] = {},
+                ["core.looking-glass"] = {},
+                ["core.presenter"] = {
+                    config = {
+                        zen_mode = "zen-mode",
+                    }
+                },
+                ["core.tangle"] = {
+                    config = {
+                        report_on_empty = false,
+                    }
+                },
+                ["core.tempus"] = {},
+                ["core.ui.calendar"] = {},
+                ["external.agenda"] = {},
+                ["external.roam"] = {
+                    config = {
+                        fuzzy_finder = "Fzf",
+                        fuzzy_backlinks = false,
+                        roam_base_directory = "vault"
+                    }
+                },
+                ["external.many-mans"] = {
+                    config = {
+                        metadata_fold = true,
+                        code_fold = false,
+                    }
+                },
+            },
+        })
 
-            require("neorg.core").modules.get_module("core.dirman").set_workspace("default")
+        require("neorg.core").modules.get_module("core.dirman").set_workspace("default")
 
-            vim.keymap.set("n", "<leader>NT", "<cmd>Neorg cycle_task<CR>",
-                { noremap = true, silent = true, desc = "Cycle task" })
-            vim.keymap.set("n", "<leader>N_", "<cmd>Neorg update_property_metadata<CR>",
-                { noremap = true, silent = true, desc = "Update property metadata" })
-            vim.keymap.set("n", "<leader>NFB", "<cmd>Neorg roam backlinks<cr>",
-                { noremap = true, silent = true, desc = "Backlinks" })
-            vim.keymap.set("n", "<leader>Nw", "<cmd>Neorg roam select_workspace<cr>",
-                { noremap = true, silent = true, desc = "Workspaces" })
-            vim.keymap.set("n", "<leader>NFb", "<cmd>Neorg roam block<cr>",
-                { noremap = true, silent = true, desc = "Block Injector" })
-            vim.keymap.set("n", "<leader>NFn", "<cmd>Neorg roam node<cr>",
-                { noremap = true, silent = true, desc = "Node Injector" })
-            vim.keymap.set("n", "<leader>NAd", "<cmd>Neorg agenda day<cr>",
-                { noremap = true, silent = true, desc = "Neorg Agenda Day" })
-            vim.keymap.set("n", "<leader>NAp", "<cmd>Neorg agenda page undone pending hold<cr>",
-                { noremap = true, silent = true, desc = "Neorg Agenda Page" })
-            vim.keymap.set("n", "<leader>NAt", "<cmd>Neorg agenda tag<cr>",
-                { noremap = true, silent = true, desc = "Neorg Agenda Tag" })
-            vim.keymap.set("n", "<leader>Nt", "<cmd>Neorg toc<cr>",
-                { noremap = true, silent = true, desc = "Table of Contents" })
-        end,
-    },
+        vim.keymap.set("n", "<leader>NT", "<cmd>Neorg cycle_task<CR>",
+            { noremap = true, silent = true, desc = "Cycle task" })
+        vim.keymap.set("n", "<leader>N_", "<cmd>Neorg update_property_metadata<CR>",
+            { noremap = true, silent = true, desc = "Update property metadata" })
+        vim.keymap.set("n", "<leader>NFB", "<cmd>Neorg roam backlinks<cr>",
+            { noremap = true, silent = true, desc = "Backlinks" })
+        vim.keymap.set("n", "<leader>Nw", "<cmd>Neorg roam select_workspace<cr>",
+            { noremap = true, silent = true, desc = "Workspaces" })
+        vim.keymap.set("n", "<leader>NFb", "<cmd>Neorg roam block<cr>",
+            { noremap = true, silent = true, desc = "Block Injector" })
+        vim.keymap.set("n", "<leader>NFn", "<cmd>Neorg roam node<cr>",
+            { noremap = true, silent = true, desc = "Node Injector" })
+        vim.keymap.set("n", "<leader>NAd", "<cmd>Neorg agenda day<cr>",
+            { noremap = true, silent = true, desc = "Neorg Agenda Day" })
+        vim.keymap.set("n", "<leader>NAp", "<cmd>Neorg agenda page undone pending hold<cr>",
+            { noremap = true, silent = true, desc = "Neorg Agenda Page" })
+        vim.keymap.set("n", "<leader>NAt", "<cmd>Neorg agenda tag<cr>",
+            { noremap = true, silent = true, desc = "Neorg Agenda Tag" })
+        vim.keymap.set("n", "<leader>Nt", "<cmd>Neorg toc<cr>",
+            { noremap = true, silent = true, desc = "Table of Contents" })
+    end,
 }
