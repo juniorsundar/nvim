9975848096c48f7c483f9aef9a438637dc153d1e
juniorsundar-tii <juniorsundar-tii@users.noreply.github.com>
2024-11-25 17:57:38 +0000

    chore: autoformat with stylua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,8 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        local lualine = require('lualine')
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local lualine = require "lualine"
 
         -- Color table for highlights
         -- stylua: ignore
@@ -20,247 +20,246 @@ return {
             yellow   = '#f1ff5e',
         }
 
-        local modus_vivendi = {
-            bg       = '#121212',
-            blue     = '#4f97d7',
-            cyan     = '#00bc7f',
-            darkblue = '#081633',
-            fg       = '#ffffff',
-            green    = '#44bc44',
-            magenta  = '#d7afff',
-            orange   = '#ffa94d',
-            red      = '#ff8059',
-            violet   = '#a899ff',
-            yellow   = '#e5c07b',
-        }
+    local modus_vivendi = {
+      bg = "#121212",
+      blue = "#4f97d7",
+      cyan = "#00bc7f",
+      darkblue = "#081633",
+      fg = "#ffffff",
+      green = "#44bc44",
+      magenta = "#d7afff",
+      orange = "#ffa94d",
+      red = "#ff8059",
+      violet = "#a899ff",
+      yellow = "#e5c07b",
+    }
 
-        local colors = cyberdream
+    local colors = cyberdream
 
-        local conditions = {
-            buffer_not_empty = function()
-                return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
-            end,
-            hide_in_width = function()
-                return vim.fn.winwidth(0) > 80
-            end,
-            check_git_workspace = function()
-                local filepath = vim.fn.expand('%:p:h')
-                local gitdir = vim.fn.finddir('.git', filepath .. ';')
-                return gitdir and #gitdir > 0 and #gitdir < #filepath
-            end,
-        }
+    local conditions = {
+      buffer_not_empty = function()
+        return vim.fn.empty(vim.fn.expand "%:t") ~= 1
+      end,
+      hide_in_width = function()
+        return vim.fn.winwidth(0) > 80
+      end,
+      check_git_workspace = function()
+        local filepath = vim.fn.expand "%:p:h"
+        local gitdir = vim.fn.finddir(".git", filepath .. ";")
+        return gitdir and #gitdir > 0 and #gitdir < #filepath
+      end,
+    }
 
-        -- Config
-        local config = {
-            options = {
-                disabled_filetypes = {
-                    "NeogitStatus",
-                    "snacks_dashboard",
-                    "oil",
-                    "ministarter",
-                    "fzf",
-                },
-                -- Disable sections and component separators
-                component_separators = '',
-                section_separators = '',
-                theme = {
-                    -- We are going to use lualine_c an lualine_x as left and
-                    -- right section. Both are highlighted by c theme .  So we
-                    -- are just setting default looks o statusline
-                    normal = { c = { fg = colors.fg, bg = colors.bg } },
-                    inactive = { c = { fg = colors.fg, bg = colors.bg } },
-                },
-            },
-            sections = {
-                -- these are to remove the defaults
-                lualine_a = {},
-                lualine_b = {},
-                lualine_y = {},
-                lualine_z = {},
-                -- These will be filled later
-                lualine_c = {},
-                lualine_x = {},
-            },
-            inactive_sections = {
-                -- these are to remove the defaults
-                lualine_a = {},
-                lualine_b = {},
-                lualine_y = {},
-                lualine_z = {},
-                lualine_c = { 'filename' },
-                lualine_x = { 'location' },
-            },
-        }
+    -- Config
+    local config = {
+      options = {
+        disabled_filetypes = {
+          "NeogitStatus",
+          "snacks_dashboard",
+          "oil",
+          "ministarter",
+          "fzf",
+        },
+        -- Disable sections and component separators
+        component_separators = "",
+        section_separators = "",
+        theme = {
+          -- We are going to use lualine_c an lualine_x as left and
+          -- right section. Both are highlighted by c theme .  So we
+          -- are just setting default looks o statusline
+          normal = { c = { fg = colors.fg, bg = colors.bg } },
+          inactive = { c = { fg = colors.fg, bg = colors.bg } },
+        },
+      },
+      sections = {
+        -- these are to remove the defaults
+        lualine_a = {},
+        lualine_b = {},
+        lualine_y = {},
+        lualine_z = {},
+        -- These will be filled later
+        lualine_c = {},
+        lualine_x = {},
+      },
+      inactive_sections = {
+        -- these are to remove the defaults
+        lualine_a = {},
+        lualine_b = {},
+        lualine_y = {},
+        lualine_z = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+      },
+    }
 
-        -- Inserts a component in lualine_c at left section
-        local function ins_left(component)
-            table.insert(config.sections.lualine_c, component)
-        end
-
-        -- Inserts a component in lualine_x at right section
-        local function ins_right(component)
-            table.insert(config.sections.lualine_x, component)
-        end
-
-        ins_left {
-            function()
-                return ''
-            end,
-            color = function()
-                -- auto change color according to neovims mode
-                local mode_color = {
-                    n = colors.blue,
-                    i = colors.red,
-                    v = colors.green,
-                    [''] = colors.green,
-                    V = colors.green,
-                    c = colors.magenta,
-                    no = colors.red,
-                    s = colors.orange,
-                    S = colors.orange,
-                    [''] = colors.orange,
-                    ic = colors.yellow,
-                    R = colors.violet,
-                    Rv = colors.violet,
-                    cv = colors.red,
-                    ce = colors.red,
-                    r = colors.cyan,
-                    rm = colors.cyan,
-                    ['r?'] = colors.cyan,
-                    ['!'] = colors.red,
-                    t = colors.red,
-                }
-                return { bg = mode_color[vim.fn.mode()] }
-            end,
-            padding = { left = 0, right = 1 }, -- We don't need space before this
-        }
-
-        ins_left {
-            -- mode component
-            -- function()
-            --     return ''
-            -- end,
-            "mode",
-            color = function()
-                -- auto change color according to neovims mode
-                local mode_color = {
-                    n = colors.blue,
-                    i = colors.red,
-                    v = colors.green,
-                    [''] = colors.green,
-                    V = colors.green,
-                    c = colors.magenta,
-                    no = colors.red,
-                    s = colors.orange,
-                    S = colors.orange,
-                    [''] = colors.orange,
-                    ic = colors.yellow,
-                    R = colors.violet,
-                    Rv = colors.violet,
-                    cv = colors.red,
-                    ce = colors.red,
-                    r = colors.cyan,
-                    rm = colors.cyan,
-                    ['r?'] = colors.cyan,
-                    ['!'] = colors.red,
-                    t = colors.red,
-                }
-                return { bg = mode_color[vim.fn.mode()], fg = colors.bg }
-            end,
-            padding = { left = 1, right = 1 },
-        }
-
-        ins_left {
-            'branch',
-            icon = '',
-            color = { fg = colors.violet, gui = 'bold' },
-            padding = { left = 1, right = 1 },
-        }
-
-        ins_left {
-            'diagnostics',
-            sources = { 'nvim_diagnostic' },
-            symbols = { error = ' ', warn = ' ', info = ' ' },
-            diagnostics_color = {
-                error = { fg = colors.red },
-                warn = { fg = colors.yellow },
-                info = { fg = colors.cyan },
-            },
-        }
-
-        -- ins_left {
-        --     'diff',
-        --     -- Is it me or the symbol for modified us really weird
-        --     symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
-        --     diff_color = {
-        --         added = { fg = colors.green },
-        --         modified = { fg = colors.orange },
-        --         removed = { fg = colors.red },
-        --     },
-        --     cond = conditions.hide_in_width,
-        --     padding = { right = 1 }
-        -- }
-
-        ins_left {
-            'filename',
-            cond = conditions.buffer_not_empty,
-            color = { fg = colors.fg, gui = 'bold' },
-        }
-
-
-        -- Insert mid section. You can make any number of sections in neovim :)
-        -- for lualine it's any number greater then 2
-        ins_left {
-            function()
-                return '%='
-            end,
-        }
-
-        -- Add components to right sections
-        ins_right {
-            'filetype',
-            icons_enabled = true, -- I think icons are cool but Eviline doesn't have them. sigh
-            color = { gui = 'italic' }
-        }
-
-        ins_right { 'location' }
-
-        ins_right { 'progress', color = { fg = colors.fg, gui = 'bold' } }
-
-        ins_right {
-            function()
-                return '▊'
-            end,
-            color = function()
-                -- auto change color according to neovims mode
-                local mode_color = {
-                    n = colors.blue,
-                    i = colors.red,
-                    v = colors.green,
-                    [''] = colors.green,
-                    V = colors.green,
-                    c = colors.magenta,
-                    no = colors.red,
-                    s = colors.orange,
-                    S = colors.orange,
-                    [''] = colors.orange,
-                    ic = colors.yellow,
-                    R = colors.violet,
-                    Rv = colors.violet,
-                    cv = colors.red,
-                    ce = colors.red,
-                    r = colors.cyan,
-                    rm = colors.cyan,
-                    ['r?'] = colors.cyan,
-                    ['!'] = colors.red,
-                    t = colors.red,
-                }
-                return { fg = mode_color[vim.fn.mode()] }
-            end,
-            padding = { left = 1 },
-        }
-
-        -- Now don't forget to initialize lualine
-        lualine.setup(config)
+    -- Inserts a component in lualine_c at left section
+    local function ins_left(component)
+      table.insert(config.sections.lualine_c, component)
     end
+
+    -- Inserts a component in lualine_x at right section
+    local function ins_right(component)
+      table.insert(config.sections.lualine_x, component)
+    end
+
+    ins_left {
+      function()
+        return ""
+      end,
+      color = function()
+        -- auto change color according to neovims mode
+        local mode_color = {
+          n = colors.blue,
+          i = colors.red,
+          v = colors.green,
+          [""] = colors.green,
+          V = colors.green,
+          c = colors.magenta,
+          no = colors.red,
+          s = colors.orange,
+          S = colors.orange,
+          [""] = colors.orange,
+          ic = colors.yellow,
+          R = colors.violet,
+          Rv = colors.violet,
+          cv = colors.red,
+          ce = colors.red,
+          r = colors.cyan,
+          rm = colors.cyan,
+          ["r?"] = colors.cyan,
+          ["!"] = colors.red,
+          t = colors.red,
+        }
+        return { bg = mode_color[vim.fn.mode()] }
+      end,
+      padding = { left = 0, right = 1 }, -- We don't need space before this
+    }
+
+    ins_left {
+      -- mode component
+      -- function()
+      --     return ''
+      -- end,
+      "mode",
+      color = function()
+        -- auto change color according to neovims mode
+        local mode_color = {
+          n = colors.blue,
+          i = colors.red,
+          v = colors.green,
+          [""] = colors.green,
+          V = colors.green,
+          c = colors.magenta,
+          no = colors.red,
+          s = colors.orange,
+          S = colors.orange,
+          [""] = colors.orange,
+          ic = colors.yellow,
+          R = colors.violet,
+          Rv = colors.violet,
+          cv = colors.red,
+          ce = colors.red,
+          r = colors.cyan,
+          rm = colors.cyan,
+          ["r?"] = colors.cyan,
+          ["!"] = colors.red,
+          t = colors.red,
+        }
+        return { bg = mode_color[vim.fn.mode()], fg = colors.bg }
+      end,
+      padding = { left = 1, right = 1 },
+    }
+
+    ins_left {
+      "branch",
+      icon = "",
+      color = { fg = colors.violet, gui = "bold" },
+      padding = { left = 1, right = 1 },
+    }
+
+    ins_left {
+      "diagnostics",
+      sources = { "nvim_diagnostic" },
+      symbols = { error = " ", warn = " ", info = " " },
+      diagnostics_color = {
+        error = { fg = colors.red },
+        warn = { fg = colors.yellow },
+        info = { fg = colors.cyan },
+      },
+    }
+
+    -- ins_left {
+    --     'diff',
+    --     -- Is it me or the symbol for modified us really weird
+    --     symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
+    --     diff_color = {
+    --         added = { fg = colors.green },
+    --         modified = { fg = colors.orange },
+    --         removed = { fg = colors.red },
+    --     },
+    --     cond = conditions.hide_in_width,
+    --     padding = { right = 1 }
+    -- }
+
+    ins_left {
+      "filename",
+      cond = conditions.buffer_not_empty,
+      color = { fg = colors.fg, gui = "bold" },
+    }
+
+    -- Insert mid section. You can make any number of sections in neovim :)
+    -- for lualine it's any number greater then 2
+    ins_left {
+      function()
+        return "%="
+      end,
+    }
+
+    -- Add components to right sections
+    ins_right {
+      "filetype",
+      icons_enabled = true, -- I think icons are cool but Eviline doesn't have them. sigh
+      color = { gui = "italic" },
+    }
+
+    ins_right { "location" }
+
+    ins_right { "progress", color = { fg = colors.fg, gui = "bold" } }
+
+    ins_right {
+      function()
+        return "▊"
+      end,
+      color = function()
+        -- auto change color according to neovims mode
+        local mode_color = {
+          n = colors.blue,
+          i = colors.red,
+          v = colors.green,
+          [""] = colors.green,
+          V = colors.green,
+          c = colors.magenta,
+          no = colors.red,
+          s = colors.orange,
+          S = colors.orange,
+          [""] = colors.orange,
+          ic = colors.yellow,
+          R = colors.violet,
+          Rv = colors.violet,
+          cv = colors.red,
+          ce = colors.red,
+          r = colors.cyan,
+          rm = colors.cyan,
+          ["r?"] = colors.cyan,
+          ["!"] = colors.red,
+          t = colors.red,
+        }
+        return { fg = mode_color[vim.fn.mode()] }
+      end,
+      padding = { left = 1 },
+    }
+
+    -- Now don't forget to initialize lualine
+    lualine.setup(config)
+  end,
 }
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,153 +1,151 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        config = function()
-            local modus_vivendi = {
-                color_overrides = {
-                    macchiato = {
-                        base = "#000000",      -- Background
-                        blue = "#4f97d7",      -- Bright blue
-                        crust = "#1e1e1e",     -- Dark crust (similar to bg_highlight)
-                        flamingo = "#f8c7c3",  -- Flamingo equivalent
-                        green = "#44bc44",     -- Bright green
-                        lavender = "#d7afff",  -- Light purple
-                        mantle = "#121212",    -- Darker background
-                        maroon = "#ff6a6a",    -- Maroon equivalent
-                        mauve = "#a899ff",     -- Mauve equivalent
-                        overlay0 = "#7c7c7c",  -- Overlay0 equivalent
-                        overlay1 = "#8c8c8c",  -- Overlay1 equivalent
-                        overlay2 = "#9c9c9c",  -- Overlay2 equivalent
-                        peach = "#ffa94d",     -- Bright peach/orange
-                        pink = "#d7a3ff",      -- Bright pink
-                        red = "#ff8059",       -- Bright red
-                        rosewater = "#fae3e0", -- Light red/rosewater equivalent
-                        sapphire = "#1fa3cf",  -- Sapphire equivalent
-                        sky = "#00f5e9",       -- Sky/cyan equivalent
-                        subtext0 = "#bcbcbc",  -- Subtext0 equivalent
-                        subtext1 = "#dcdcdc",  -- Subtext1 equivalent
-                        surface0 = "#363a4f",  -- Surface0 equivalent
-                        surface1 = "#494d64",  -- Surface1 equivalent
-                        surface2 = "#5b6078",  -- Surface2 equivalent
-                        teal = "#00bc7f",      -- Teal equivalent
-                        text = "#ffffff",      -- Foreground (bright text)
-                        yellow = "#e5c07b",    -- Yellow equivalent
-                    },
-                },
-                custom_highlights = function(colors)
-                    return {
-                    }
-                end,
-                highlight_overrides = {
-                    macchiato = function(colors)
-                        return {
-                            NormalFloat = { fg = colors.text, bg = colors.mantle },
-                            Comment = { fg = colors.overlay1 },
-                            LineNr = { fg = colors.overlay1 },
-                            CursorLineNr = { fg = colors.text },
-                            WhichKey = { fg = colors.text, bg = colors.base },
-                            WhichKeyNormal = { fg = colors.text, bg = colors.base },
-                            Folded = { fg = colors.crust, bg = colors.none },
-                            ["@neorg.tags.ranged_verbatim.code_block"] = { bg = colors.crust }
-                        }
-                    end,
-                },
-                transparent_background = false,
-                integrations = {
-                    cmp = true,
-                    gitsigns = true,
-                    treesitter = false,
-                    -- markdown = true,
-                    neogit = true,
-                    treesitter_context = true,
-                    lsp_saga = true,
-                    diffview = true,
-                    which_key = true,
-                    mason = true,
-                    notify = true,
-                    native_lsp = {
-                        enabled = true,
-                    },
-                    mini = true,
-                    nvim_surround = true
-                },
-            }
-            local cyberdream = {
-                color_overrides = {
-                    macchiato = {
-                        base = "#16181A",
-                        blue = "#5EA1FF",
-                        crust = "#3C4048",
-                        flamingo = "#FF9F9F",
-                        green = "#5EFF6C",
-                        lavender = "#FF5EF1",
-                        mantle = "#1E2124",
-                        maroon = "#D96666",
-                        mauve = "#BD5EFF",
-                        overlay0 = "#474A55",
-                        overlay1 = "#5A5E6B",
-                        overlay2 = "#6E7280",
-                        peach = "#FFBD5E",
-                        pink = "#FF5EA0",
-                        red = "#FF6E5E",
-                        rosewater = "#FFD1DC",
-                        sapphire = "#4A90E2",
-                        sky = "#5EF1FF",
-                        subtext0 = "#8A8E99",
-                        subtext1 = "#A0A4B8",
-                        surface0 = "#26282E",
-                        surface1 = "#2E3138",
-                        surface2 = "#363940",
-                        teal = "#64D8CB",
-                        text = "#FFFFFF",
-                        yellow = "#F1FF5E",
-                    },
-                },
-                custom_highlights = function(colors)
-                    return {
-                    }
-                end,
-                highlight_overrides = {
-                    macchiato = function(colors)
-                        return {
-                            NormalFloat = { fg = colors.text, bg = colors.base },
-                            Comment = { fg = colors.overlay1 },
-                            LineNr = { fg = colors.overlay1 },
-                            CursorLineNr = { fg = colors.text },
-                            WhichKey = { fg = colors.text, bg = colors.base },
-                            WhichKeyNormal = { fg = colors.text, bg = colors.base },
-                            Folded = { fg = colors.crust, bg = colors.none },
-                        }
-                    end,
-                },
-                transparent_background = false,
-                integrations = {
-                    cmp = true,
-                    gitsigns = true,
-                    treesitter = false,
-                    -- markdown = true,
-                    neogit = true,
-                    snacks = true,
-                    treesitter_context = true,
-                    lsp_saga = true,
-                    diffview = true,
-                    which_key = true,
-                    mason = true,
-                    notify = true,
-                    native_lsp = {
-                        enabled = true,
-                    },
-                    mini = true,
-                    nvim_surround = true
-                },
-            }
-
-            require("catppuccin").setup(cyberdream)
-            -- require("catppuccin").setup(modus_vivendi)
-            vim.cmd.colorscheme("catppuccin-macchiato")
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      local modus_vivendi = {
+        color_overrides = {
+          macchiato = {
+            base = "#000000", -- Background
+            blue = "#4f97d7", -- Bright blue
+            crust = "#1e1e1e", -- Dark crust (similar to bg_highlight)
+            flamingo = "#f8c7c3", -- Flamingo equivalent
+            green = "#44bc44", -- Bright green
+            lavender = "#d7afff", -- Light purple
+            mantle = "#121212", -- Darker background
+            maroon = "#ff6a6a", -- Maroon equivalent
+            mauve = "#a899ff", -- Mauve equivalent
+            overlay0 = "#7c7c7c", -- Overlay0 equivalent
+            overlay1 = "#8c8c8c", -- Overlay1 equivalent
+            overlay2 = "#9c9c9c", -- Overlay2 equivalent
+            peach = "#ffa94d", -- Bright peach/orange
+            pink = "#d7a3ff", -- Bright pink
+            red = "#ff8059", -- Bright red
+            rosewater = "#fae3e0", -- Light red/rosewater equivalent
+            sapphire = "#1fa3cf", -- Sapphire equivalent
+            sky = "#00f5e9", -- Sky/cyan equivalent
+            subtext0 = "#bcbcbc", -- Subtext0 equivalent
+            subtext1 = "#dcdcdc", -- Subtext1 equivalent
+            surface0 = "#363a4f", -- Surface0 equivalent
+            surface1 = "#494d64", -- Surface1 equivalent
+            surface2 = "#5b6078", -- Surface2 equivalent
+            teal = "#00bc7f", -- Teal equivalent
+            text = "#ffffff", -- Foreground (bright text)
+            yellow = "#e5c07b", -- Yellow equivalent
+          },
+        },
+        custom_highlights = function(colors)
+          return {}
         end,
-    },
-    {
-        "nvim-tree/nvim-web-devicons",
-    },
+        highlight_overrides = {
+          macchiato = function(colors)
+            return {
+              NormalFloat = { fg = colors.text, bg = colors.mantle },
+              Comment = { fg = colors.overlay1 },
+              LineNr = { fg = colors.overlay1 },
+              CursorLineNr = { fg = colors.text },
+              WhichKey = { fg = colors.text, bg = colors.base },
+              WhichKeyNormal = { fg = colors.text, bg = colors.base },
+              Folded = { fg = colors.crust, bg = colors.none },
+              ["@neorg.tags.ranged_verbatim.code_block"] = { bg = colors.crust },
+            }
+          end,
+        },
+        transparent_background = false,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          treesitter = false,
+          -- markdown = true,
+          neogit = true,
+          treesitter_context = true,
+          lsp_saga = true,
+          diffview = true,
+          which_key = true,
+          mason = true,
+          notify = true,
+          native_lsp = {
+            enabled = true,
+          },
+          mini = true,
+          nvim_surround = true,
+        },
+      }
+      local cyberdream = {
+        color_overrides = {
+          macchiato = {
+            base = "#16181A",
+            blue = "#5EA1FF",
+            crust = "#3C4048",
+            flamingo = "#FF9F9F",
+            green = "#5EFF6C",
+            lavender = "#FF5EF1",
+            mantle = "#1E2124",
+            maroon = "#D96666",
+            mauve = "#BD5EFF",
+            overlay0 = "#474A55",
+            overlay1 = "#5A5E6B",
+            overlay2 = "#6E7280",
+            peach = "#FFBD5E",
+            pink = "#FF5EA0",
+            red = "#FF6E5E",
+            rosewater = "#FFD1DC",
+            sapphire = "#4A90E2",
+            sky = "#5EF1FF",
+            subtext0 = "#8A8E99",
+            subtext1 = "#A0A4B8",
+            surface0 = "#26282E",
+            surface1 = "#2E3138",
+            surface2 = "#363940",
+            teal = "#64D8CB",
+            text = "#FFFFFF",
+            yellow = "#F1FF5E",
+          },
+        },
+        custom_highlights = function(colors)
+          return {}
+        end,
+        highlight_overrides = {
+          macchiato = function(colors)
+            return {
+              NormalFloat = { fg = colors.text, bg = colors.base },
+              Comment = { fg = colors.overlay1 },
+              LineNr = { fg = colors.overlay1 },
+              CursorLineNr = { fg = colors.text },
+              WhichKey = { fg = colors.text, bg = colors.base },
+              WhichKeyNormal = { fg = colors.text, bg = colors.base },
+              Folded = { fg = colors.crust, bg = colors.none },
+            }
+          end,
+        },
+        transparent_background = false,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          treesitter = false,
+          -- markdown = true,
+          neogit = true,
+          snacks = true,
+          treesitter_context = true,
+          lsp_saga = true,
+          diffview = true,
+          which_key = true,
+          mason = true,
+          notify = true,
+          native_lsp = {
+            enabled = true,
+          },
+          mini = true,
+          nvim_surround = true,
+        },
+      }
+
+      require("catppuccin").setup(cyberdream)
+      -- require("catppuccin").setup(modus_vivendi)
+      vim.cmd.colorscheme "catppuccin-macchiato"
+    end,
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+  },
 }
