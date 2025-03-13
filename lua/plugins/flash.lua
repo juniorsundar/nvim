46fd44bb8fe4b3d456b69d03b239c0b269131ceb
Junior Sundar <juniorsundar@gmail.com>
2025-03-13 21:23:29 +0400

    Change flash keymap

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,7 +2,7 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     keys = {
-        { "<C-s>",   mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+        { "s",       mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
         { "<C-M-s>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
         { "gr",      mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
         { "gR",      mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
