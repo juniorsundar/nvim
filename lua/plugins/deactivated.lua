0b6298a2aaa60dfbb4d118b96ff91432dab167c3
Junior Sundar <juniorsundar@gmail.com>
2025-03-19 23:41:24 +0400

    Revert to neogit

--- lua/plugins/deactivated.lua
+++ lua/plugins/deactivated.lua
@@ -1,17 +1,10 @@
 return {
     {},
     -- {
-    --     "folke/flash.nvim",
-    --     event = "VeryLazy",
+    --     "tpope/vim-fugitive",
+    --     lazy = false,
     --     keys = {
-    --         { "<C-s>",   mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-    --         { "<C-M-s>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-    --         { "gr",      mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-    --         { "gR",      mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    --     },
-    --     config = function()
-    --         require("flash").setup()
-    --     end
+    --         { "<leader>Gg", "<cmd>tab Git<cr>", desc = "Fugitive" }
     -- },
     -- {
     --     "OXY2DEV/markview.nvim",
