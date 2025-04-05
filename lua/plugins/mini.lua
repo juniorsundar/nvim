2b652576435c7f63f064bf77285b9ad824059d5a
Junior Sundar <juniorsundar@gmail.com>
2025-04-05 20:37:35 +0400

    Update mini
    Update lock

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,14 +3,28 @@ return {
         "echasnovski/mini.pairs",
         event = "VeryLazy",
         config = function()
-            require("mini.pairs").setup()
+            require("mini.pairs").setup({
+                modes = { insert = true, command = true, terminal = false },
+                -- skip autopair when next character is one of these
+                skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+                -- skip autopair when the cursor is inside these treesitter nodes
+                skip_ts = { "string" },
+                -- skip autopair when next character is closing pair
+                -- and there are more closing pairs than opening pairs
+                skip_unbalanced = true,
+                -- better deal with markdown code blocks
+                markdown = true,
+            })
         end,
     },
     {
         "echasnovski/mini.ai",
         event = "VeryLazy",
         config = function()
-            require("mini.ai").setup()
+            local ai = require("mini.ai")
+            ai.setup({
+                n_lines = 500,
+            })
         end,
     },
     {
