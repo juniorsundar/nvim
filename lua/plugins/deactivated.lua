7b154fe930800ea8bdcb3b91b940284512454d42
Junior Sundar <juniorsundar@gmail.com>
2025-04-16 21:58:06 +0400

    Tested obsidian

--- lua/plugins/deactivated.lua
+++ lua/plugins/deactivated.lua
@@ -334,46 +334,64 @@ return {
     --     end,
     -- },
     -- {
-    --     "epwalsh/obsidian.nvim",
-    --     version = "*", -- recommended, use latest release instead of latest commit
-    --     lazy = true,
-    --     ft = "markdown",
-    --     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    --     -- event = {
-    --     --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --     --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --     --   -- refer to `:h file-pattern` for more examples
-    --     --   "BufReadPre path/to/my-vault/*.md",
-    --     --   "BufNewFile path/to/my-vault/*.md",
-    --     -- },
+    --     "obsidian-nvim/obsidian.nvim",
+    --     version = "*",
     --     dependencies = {
-    --         -- Required.
     --         "nvim-lua/plenary.nvim",
-    --
-    --         -- see below for full list of optional dependencies 👇
     --     },
     --     opts = {
     --         workspaces = {
     --             {
     --                 name = "vault",
     --                 path = "~/Dropbox/obsidian/vault",
+    --                 overrides = {
+    --                     notes_subdir = "pages",
+    --                     daily_notes = {
+    --                         folder = "journals",
+    --                         template = "journal_template.md"
+    --                     },
+    --                 },
     --             },
     --             {
     --                 name = "gow",
     --                 path = "~/Dropbox/obsidian/gow",
     --             },
     --         },
-    --         ui = {
-    --             enable = false
+    --         completion = {
+    --             nvim_cmp = false,
+    --             blink = true,
+    --             min_chars = 2,
     --         },
-    --         notes_subdir = "pages",
-    --         daily_notes = {
-    --             folder = "journals",
-    --             default_tags = {"journal"}
-    --         }
+    --         picker = {
+    --             name = "snacks.pick",
+    --             note_mappings = {
+    --                 new = "<C-x>",
+    --                 insert_link = "<C-l>",
+    --             },
+    --             tag_mappings = {
+    --                 tag_note = "<C-x>",
+    --                 insert_tag = "<C-l>",
+    --             },
+    --         },
+    --         templates = {
+    --             folder = "templates",
+    --             date_format = "%Y-%m-%d",
+    --             time_format = "%H:%M",
+    --             substitutions = {},
+    --         },
+    --         disable_frontmatter = true,
+    --         attachments = {
+    --             img_folder = "assets",
+    --             img_name_func = function()
+    --                 return string.format("%s-", os.time())
+    --             end,
     --
-    --         -- see below for full list of options 👇
-    --     },
+    --             img_text_func = function(client, path)
+    --                 path = client:vault_relative_path(path) or path
+    --                 return string.format("![%s](%s)", path.name, path)
+    --             end,
+    --         },
+    --     }
     -- },
     -- {
     --     'nvim-telescope/telescope.nvim',
