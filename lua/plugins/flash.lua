9e723c6d458ba59959ba263d561044d4c1b130c8
Junior Sundar <juniorsundar@gmail.com>
2025-07-18 13:27:13 +0400

    C-s for navigating in Flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,7 +2,7 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     keys = {
-        { "s",       mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+        { "<C-s>",       mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
         { "<C-M-s>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
         { "gr",      mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
         { "gR",      mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
