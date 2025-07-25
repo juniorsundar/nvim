b2a103906d7594ba45b1ca3dd0c84f3aee90d749
Junior Sundar <juniorsundar@gmail.com>
2025-07-25 22:11:41 +0400

    Added more plugins

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,18 @@
+return {
+  "akinsho/bufferline.nvim",
+  enabled = false,
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("bufferline").setup {
+      options = {
+        themable = true,
+        indicator = {
+          icon = "▎",
+          style = "icon",
+        },
+        separator_style = "thick",
+        diagnostics = "nvim_lsp",
+      },
+    }
+  end,
+}
