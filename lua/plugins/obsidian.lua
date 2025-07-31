cae52f299bb6040899a9a173a8ef0c6f272ca46d
Junior Sundar <juniorsundar@gmail.com>
2025-07-31 13:07:09 +0400

    Obsidian keymaps

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -2,58 +2,75 @@ return {
   "obsidian-nvim/obsidian.nvim",
   enabled = true,
   version = "*",
-  opts = {
-    legacy_commands = false,
-    workspaces = {
-      {
-        name = "vault",
-        path = "~/Dropbox/obsidian/vault",
-        overrides = {
-          notes_subdir = "pages",
-          daily_notes = {
-            folder = "journals",
-            template = "journal_template.md",
+  config = function()
+    require("obsidian").setup {
+      legacy_commands = false,
+      workspaces = {
+        {
+          name = "vault",
+          path = "~/Dropbox/obsidian/vault",
+          overrides = {
+            notes_subdir = "pages",
+            daily_notes = {
+              folder = "journals",
+              template = "journal_template.md",
+            },
           },
         },
+        {
+          name = "gow",
+          path = "~/Dropbox/obsidian/gow",
+        },
       },
-      {
-        name = "gow",
-        path = "~/Dropbox/obsidian/gow",
+      completion = {
+        nvim_cmp = false,
+        blink = true,
+        min_chars = 1,
       },
-    },
-    completion = {
-      nvim_cmp = false,
-      blink = true,
-      min_chars = 1,
-    },
-    picker = {
-      name = "snacks.pick",
-      note_mappings = {
-        new = "<C-x>",
-        insert_link = "<C-l>",
+      picker = {
+        name = "snacks.pick",
+        note_mappings = {
+          new = "<C-x>",
+          insert_link = "<C-l>",
+        },
+        tag_mappings = {
+          tag_note = "<C-x>",
+          insert_tag = "<C-l>",
+        },
       },
-      tag_mappings = {
-        tag_note = "<C-x>",
-        insert_tag = "<C-l>",
+      templates = {
+        folder = "templates",
+        date_format = "%Y-%m-%d",
+        time_format = "%H:%M",
+        substitutions = {},
       },
-    },
-    templates = {
-      folder = "templates",
-      date_format = "%Y-%m-%d",
-      time_format = "%H:%M",
-      substitutions = {},
-    },
-    disable_frontmatter = true,
-    attachments = {
-      img_folder = "assets",
-      img_name_func = function()
-        return string.format("%s-", os.time())
-      end,
+      ui = {
+        enable = false,
+      },
+      disable_frontmatter = true,
+      attachments = {
+        img_folder = "assets",
+        img_name_func = function()
+          return string.format("%s-", os.time())
+        end,
 
-      img_text_func = function(client, path)
-        path = client:vault_relative_path(path) or path
-        return string.format("![%s](%s)", path.name, path)
-      end,
-    },
-  },
+        img_text_func = function(client, path)
+          path = client:vault_relative_path(path) or path
+          return string.format("![%s](%s)", path.name, path)
+        end,
+      },
+    }
+    require("which-key").add {
+      { "<localleader>O", group = "Obsidian" },
+      { "<localleader>OD", group = "Dailies" },
+      { "<localleader>Oo", "<cmd>Obsidian quick_switch<cr>", desc = "Quick Switch" },
+      { "<localleader>Of", "<cmd>Obsidian search<cr>", desc = "Search" },
+      { "<localleader>Ot", "<cmd>Obsidian tags<cr>", desc = "Tags" },
+      { "<localleader>Ol", "<cmd>Obsidian links<cr>", desc = "Links" },
+      { "<localleader>Ot", "<cmd>Obsidian tags<cr>", desc = "Tags" },
+      { "<localleader>ODt", "<cmd>Obsidian today<cr>", desc = "Today" },
+      { "<localleader>ODT", "<cmd>Obsidian tomorrow<cr>", desc = "Tomorrow" },
+      { "<localleader>ODy", "<cmd>Obsidian yesterday<cr>", desc = "Yesterday" },
+    }
+  end,
 }
