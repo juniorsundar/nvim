4b865ce8e0ac1a12969f0302ba09945df433a6a9
Junior Sundar <juniorsundar@gmail.com>
2025-08-08 09:59:48 +0400

    Custom checkboxes for Rendermarkdown

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -12,6 +12,12 @@ return {
         blink = { enabled = true },
       },
       render_modes = true,
+      checkbox = {
+        custom = {
+          cancelled = {raw = '[-]', rendered = '', highlight = 'RenderMarkdownDash', scope_highlight = 'RenderMarkdownDash'},
+          doing = {raw = '[/]', rendered = '󰥔', highlight = 'RenderMarkdownTodo', scope_highlight = nil},
+        }
+      }
     }
   end,
 }
