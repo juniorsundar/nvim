ba8ee94faf224b668f63d03370b7b5d09402bebe
Junior Sundar <juniorsundar@gmail.com>
2025-08-26 19:14:03 +0400

    Trying to disable tree-sitter when not available

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,45 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
+  enabled = true,
   branch = "main",
   build = ":TSUpdate",
-  init = function()
+  config = function()
+    require'nvim-treesitter'.install {
+        "rust",
+        "c3",
+        "c",
+        "cpp",
+        "javascript",
+        "python",
+        "lua",
+        "yaml",
+        "markdown",
+        "nix",
+        "json",
+        "toml",
+        "go",
+        "gomod",
+        "zig",
+    }
+
     vim.api.nvim_create_autocmd("FileType", {
       pattern = {
         "rust",
         "c3",
         "c",
         "cpp",
+        "javascript",
         "python",
         "lua",
         "yaml",
         "markdown",
-        "nix"
+        "nix",
+        "json",
+        "toml",
+        "go",
+        "gomod",
+        "zig",
       },
       callback = function()
         vim.treesitter.start()
