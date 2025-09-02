5376ae29a782ce0cae89eabab29c4e4f22e87247
Junior Sundar <juniorsundar@gmail.com>
2025-09-02 21:17:05 +0400

    Added D

---
+++ lua/.deactivated/lspsaga.lua
@@ -0,0 +1,15 @@
+return {
+  'nvimdev/lspsaga.nvim',
+  event = "LspAttach",
+  config = function()
+    require('lspsaga').setup({
+      ui = {
+        enable = false,
+        code_action = '',
+      },
+      finder = {
+        layout = 'normal',
+      }
+    })
+  end,
+}
