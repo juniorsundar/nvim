d277b9135c414947d79ce0cf535ca0692b8abe84
Junior Sundar <juniorsundar@gmail.com>
2025-09-11 23:25:03 +0400

    format
    s

--- lua/.deactivated/nvim-dap.lua
+++ lua/.deactivated/nvim-dap.lua
@@ -1,97 +1,97 @@
 return {
-  "mfussenegger/nvim-dap",
-  enabled = false,
-  dependencies = {
-    "igorlfs/nvim-dap-view",
-  },
-  event = "LspAttach",
-  config = function()
-    local dap = require "dap"
+    "mfussenegger/nvim-dap",
+    enabled = false,
+    dependencies = {
+        "igorlfs/nvim-dap-view",
+    },
+    event = "LspAttach",
+    config = function()
+        local dap = require "dap"
 
-    vim.keymap.set("n", "<leader>LAc", function()
-      dap.continue()
-    end, { desc = "Continue" })
-    vim.keymap.set("n", "<leader>LAb", function()
-      dap.toggle_breakpoint()
-    end, { desc = "Toggle Breakpoint" })
-    vim.keymap.set("n", "<leader>LAB", function()
-      dap.set_breakpoint(nil, nil, vim.fn.input "Breakpoint condition: ")
-    end, { desc = "Set Conditional Breakpoint" })
-    vim.keymap.set("n", "<leader>LAp", function()
-      dap.set_breakpoint(nil, nil, vim.fn.input "Log point message: ")
-    end, { desc = "Set Log Point" })
-    vim.keymap.set("n", "<leader>LAr", function()
-      dap.repl.open()
-    end, { desc = "Open REPL" })
-    vim.keymap.set("n", "<leader>LAl", function()
-      dap.run_last()
-    end, { desc = "Run Last" })
-    vim.keymap.set("n", "<leader>LAo", function()
-      dap.step_over()
-    end, { desc = "Step Over" })
-    vim.keymap.set("n", "<leader>LAi", function()
-      dap.step_into()
-    end, { desc = "Step Into" })
-    vim.keymap.set("n", "<leader>LAu", function()
-      dap.step_out()
-    end, { desc = "Step Out" })
-    vim.keymap.set("n", "<leader>LAt", function()
-      dap.terminate()
-    end, { desc = "Terminate" })
+        vim.keymap.set("n", "<leader>LAc", function()
+            dap.continue()
+        end, { desc = "Continue" })
+        vim.keymap.set("n", "<leader>LAb", function()
+            dap.toggle_breakpoint()
+        end, { desc = "Toggle Breakpoint" })
+        vim.keymap.set("n", "<leader>LAB", function()
+            dap.set_breakpoint(nil, nil, vim.fn.input "Breakpoint condition: ")
+        end, { desc = "Set Conditional Breakpoint" })
+        vim.keymap.set("n", "<leader>LAp", function()
+            dap.set_breakpoint(nil, nil, vim.fn.input "Log point message: ")
+        end, { desc = "Set Log Point" })
+        vim.keymap.set("n", "<leader>LAr", function()
+            dap.repl.open()
+        end, { desc = "Open REPL" })
+        vim.keymap.set("n", "<leader>LAl", function()
+            dap.run_last()
+        end, { desc = "Run Last" })
+        vim.keymap.set("n", "<leader>LAo", function()
+            dap.step_over()
+        end, { desc = "Step Over" })
+        vim.keymap.set("n", "<leader>LAi", function()
+            dap.step_into()
+        end, { desc = "Step Into" })
+        vim.keymap.set("n", "<leader>LAu", function()
+            dap.step_out()
+        end, { desc = "Step Out" })
+        vim.keymap.set("n", "<leader>LAt", function()
+            dap.terminate()
+        end, { desc = "Terminate" })
 
-    dap.adapters.codelldb = {
-      type = "executable",
-      command = "codelldb",
+        dap.adapters.codelldb = {
+            type = "executable",
+            command = "codelldb",
 
-      -- On windows you may have to uncomment this:
-      -- detached = false,
-    }
-    dap.configurations.cpp = {
-      {
-        name = "Launch file (C++)",
-        type = "codelldb", -- Matches the adapter name (mason-nvim-dap handles this)
-        request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-        cwd = "${workspaceFolder}",
-        stopOnEntry = false,
-        args = {}, -- Add command line arguments if needed
-        sourceLanguages = { "cpp" }, -- Often optional but can help
-      },
-    }
+            -- On windows you may have to uncomment this:
+            -- detached = false,
+        }
+        dap.configurations.cpp = {
+            {
+                name = "Launch file (C++)",
+                type = "codelldb", -- Matches the adapter name (mason-nvim-dap handles this)
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                end,
+                cwd = "${workspaceFolder}",
+                stopOnEntry = false,
+                args = {}, -- Add command line arguments if needed
+                sourceLanguages = { "cpp" }, -- Often optional but can help
+            },
+        }
 
-    -- Use the same structure for C and Rust, just change the name
-    dap.configurations.c = {
-      {
-        name = "Launch file (C)",
-        type = "codelldb",
-        request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-        cwd = "${workspaceFolder}",
-        stopOnEntry = false,
-        args = {},
-        sourceLanguages = { "c" },
-      },
-    }
+        -- Use the same structure for C and Rust, just change the name
+        dap.configurations.c = {
+            {
+                name = "Launch file (C)",
+                type = "codelldb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                end,
+                cwd = "${workspaceFolder}",
+                stopOnEntry = false,
+                args = {},
+                sourceLanguages = { "c" },
+            },
+        }
 
-    -- For Rust, you might want to automatically detect the target binary
-    dap.configurations.rust = {
-      {
-        name = "Launch file (Rust)",
-        type = "codelldb",
-        request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
-          -- More advanced: could try parsing Cargo.toml or using a helper function
-        end,
-        cwd = "${workspaceFolder}",
-        stopOnEntry = false,
-        args = {},
-        sourceLanguages = { "rust" },
-      },
-    }
-  end,
+        -- For Rust, you might want to automatically detect the target binary
+        dap.configurations.rust = {
+            {
+                name = "Launch file (Rust)",
+                type = "codelldb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+                    -- More advanced: could try parsing Cargo.toml or using a helper function
+                end,
+                cwd = "${workspaceFolder}",
+                stopOnEntry = false,
+                args = {},
+                sourceLanguages = { "rust" },
+            },
+        }
+    end,
 }
--- lua/.deactivated/themes.lua
+++ lua/.deactivated/themes.lua
@@ -1,155 +1,164 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    enabled = false,
-    config = function()
-      local cyberdream = {
-        base = "#16181a",
-        blue = "#5ea1ff",
-        crust = "#3c4048",
-        flamingo = "#ff9f9f",
-        green = "#5eff6c",
-        lavender = "#ff5ef1",
-        mantle = "#1e2124",
-        maroon = "#d96666",
-        mauve = "#bd5eff",
-        overlay0 = "#474a55",
-        overlay1 = "#5a5e6b",
-        overlay2 = "#6e7280",
-        peach = "#ffbd5e",
-        pink = "#ff5ea0",
-        red = "#ff6e5e",
-        rosewater = "#ffd1dc",
-        sapphire = "#4a90e2",
-        sky = "#5ef1ff",
-        subtext0 = "#8a8e99",
-        subtext1 = "#a0a4b8",
-        surface0 = "#26282e",
-        surface1 = "#2e3138",
-        surface2 = "#363940",
-        teal = "#64d8cb",
-        text = "#ffffff",
-        yellow = "#f1ff5e",
-      }
-      local config = {
-        color_overrides = {
-          macchiato = cyberdream,
-        },
-        custom_highlights = function(colors)
-          return {}
-        end,
-        highlight_overrides = {
-          macchiato = function(colors)
-            return {
-              NormalFloat = { fg = colors.text, bg = colors.base },
-              Comment = { fg = colors.overlay1 },
-              LineNr = { fg = colors.overlay1 },
-              CursorLineNr = { fg = colors.text },
-              WhichKey = { fg = colors.text, bg = colors.base },
-              WhichKeyNormal = { fg = colors.text, bg = colors.base },
-              Folded = { fg = colors.crust, bg = colors.none },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        enabled = false,
+        config = function()
+            local cyberdream = {
+                base = "#16181a",
+                blue = "#5ea1ff",
+                crust = "#3c4048",
+                flamingo = "#ff9f9f",
+                green = "#5eff6c",
+                lavender = "#ff5ef1",
+                mantle = "#1e2124",
+                maroon = "#d96666",
+                mauve = "#bd5eff",
+                overlay0 = "#474a55",
+                overlay1 = "#5a5e6b",
+                overlay2 = "#6e7280",
+                peach = "#ffbd5e",
+                pink = "#ff5ea0",
+                red = "#ff6e5e",
+                rosewater = "#ffd1dc",
+                sapphire = "#4a90e2",
+                sky = "#5ef1ff",
+                subtext0 = "#8a8e99",
+                subtext1 = "#a0a4b8",
+                surface0 = "#26282e",
+                surface1 = "#2e3138",
+                surface2 = "#363940",
+                teal = "#64d8cb",
+                text = "#ffffff",
+                yellow = "#f1ff5e",
             }
-          end,
-        },
-        transparent_background = false,
-        auto_integrations = true,
-      }
-      require("catppuccin").setup(config)
-      vim.cmd.colorscheme "catppuccin-macchiato"
-    end,
-  },
-  {
-    "Mofiqul/vscode.nvim",
-    enabled = false,
-    config = function()
-      vim.cmd.colorscheme "vscode"
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("tokyonight").setup {
-        style = "storm",
-        transparent = true,
-        lualine_bold = true,
-        styles = {
-          sidebars = "transparent",
-          floats = "transparent",
-        },
-      }
-      vim.cmd.colorscheme "tokyonight"
-    end,
-  },
-  {
-    "NTBBloodbath/doom-one.nvim",
-    enabled = false,
-    config = function()
-      vim.g.doom_one_cursor_coloring = false
-      vim.g.doom_one_terminal_colors = true
-      vim.g.doom_one_italic_comments = true
-      vim.g.doom_one_enable_treesitter = true
-      vim.g.doom_one_diagnostics_text_color = false
-      vim.g.doom_one_transparent_background = false
+            local config = {
+                color_overrides = {
+                    macchiato = cyberdream,
+                },
+                custom_highlights = function(colors)
+                    return {}
+                end,
+                highlight_overrides = {
+                    macchiato = function(colors)
+                        return {
+                            NormalFloat = { fg = colors.text, bg = colors.base },
+                            Comment = { fg = colors.overlay1 },
+                            LineNr = { fg = colors.overlay1 },
+                            CursorLineNr = { fg = colors.text },
+                            WhichKey = { fg = colors.text, bg = colors.base },
+                            WhichKeyNormal = { fg = colors.text, bg = colors.base },
+                            Folded = { fg = colors.crust, bg = colors.none },
+                        }
+                    end,
+                },
+                transparent_background = false,
+                auto_integrations = true,
+            }
+            require("catppuccin").setup(config)
+            vim.cmd.colorscheme "catppuccin-macchiato"
+        end,
+    },
+    {
+        "Mofiqul/vscode.nvim",
+        enabled = false,
+        config = function()
+            vim.cmd.colorscheme "vscode"
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        enabled = false,
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("tokyonight").setup {
+                style = "storm",
+                transparent = true,
+                lualine_bold = true,
+                styles = {
+                    sidebars = "transparent",
+                    floats = "transparent",
+                },
+            }
+            vim.cmd.colorscheme "tokyonight"
+        end,
+    },
+    {
+        "NTBBloodbath/doom-one.nvim",
+        enabled = false,
+        config = function()
+            vim.g.doom_one_cursor_coloring = false
+            vim.g.doom_one_terminal_colors = true
+            vim.g.doom_one_italic_comments = true
+            vim.g.doom_one_enable_treesitter = true
+            vim.g.doom_one_diagnostics_text_color = false
+            vim.g.doom_one_transparent_background = false
 
-      vim.g.doom_one_pumblend_enable = true
-      vim.g.doom_one_pumblend_transparency = 20
-      vim.cmd [[set fillchars+=eob:\ ]]
-      vim.cmd.colorscheme "doom-one"
-    end,
-  },
-  {
-    "vague2k/vague.nvim",
-    enabled = false,
-    priority = 1000,
-    config = function()
-      require("vague").setup {}
-    end,
-  },
-  {
-    "dgox16/oldworld.nvim",
-    lazy = false,
-    enabled = false,
-    priority = 1000,
-    config = function()
-      require("oldworld").setup {
-        terminal_colors = true,
-        variant = "default",
-        styles = {
-          comments = {},
-          keywords = {},
-          identifiers = {},
-          functions = {},
-          variables = {},
-          booleans = {},
-        },
-        integrations = {
-          alpha = true,
-          cmp = true,
-          flash = true,
-          gitsigns = true,
-          hop = false,
-          indent_blankline = true,
-          lazy = true,
-          lsp = true,
-          markdown = true,
-          mason = true,
-          navic = false,
-          neo_tree = false,
-          neogit = false,
-          neorg = false,
-          noice = true,
-          notify = true,
-          rainbow_delimiters = true,
-          telescope = true,
-          treesitter = true,
-        },
-        highlight_overrides = {},
-      }
-      vim.cmd.colorscheme "oldworld"
-    end,
-  },
+            vim.g.doom_one_pumblend_enable = true
+            vim.g.doom_one_pumblend_transparency = 20
+            vim.cmd [[set fillchars+=eob:\ ]]
+            vim.cmd.colorscheme "doom-one"
+        end,
+    },
+    {
+        "vague2k/vague.nvim",
+        enabled = false,
+        priority = 1000,
+        config = function()
+            require("vague").setup {}
+        end,
+    },
+    {
+        "dgox16/oldworld.nvim",
+        lazy = false,
+        enabled = false,
+        priority = 1000,
+        config = function()
+            require("oldworld").setup {
+                terminal_colors = true,
+                variant = "default",
+                styles = {
+                    comments = {},
+                    keywords = {},
+                    identifiers = {},
+                    functions = {},
+                    variables = {},
+                    booleans = {},
+                },
+                integrations = {
+                    alpha = true,
+                    cmp = true,
+                    flash = true,
+                    gitsigns = true,
+                    hop = false,
+                    indent_blankline = true,
+                    lazy = true,
+                    lsp = true,
+                    markdown = true,
+                    mason = true,
+                    navic = false,
+                    neo_tree = false,
+                    neogit = false,
+                    neorg = false,
+                    noice = true,
+                    notify = true,
+                    rainbow_delimiters = true,
+                    telescope = true,
+                    treesitter = true,
+                },
+                highlight_overrides = {},
+            }
+            vim.cmd.colorscheme "oldworld"
+        end,
+    },
+    {
+        "EdenEast/nightfox.nvim",
+        lazy = false,
+        enabled = false,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme nightfox")
+        end,
+    }
 }
