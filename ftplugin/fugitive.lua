81fbfc6acab1b27108dd26fc27d2a070e4359049
Junior Sundar <juniorsundar@gmail.com>
2025-09-21 22:02:18 +0400

    Buffer window number settings

--- ftplugin/fugitive.lua
+++ ftplugin/fugitive.lua
@@ -1,2 +1,19 @@
--- vim.opt.number = false
--- vim.opt.relativenumber = false
+local fugitive_augroup = vim.api.nvim_create_augroup("FugitiveSettings", { clear = true })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = fugitive_augroup,
+  pattern = "fugitive://*",
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufLeave", {
+  group = fugitive_augroup,
+  pattern = "fugitive://*",
+  callback = function()
+    vim.opt.number = true
+    vim.opt.relativenumber = true
+  end,
+})
