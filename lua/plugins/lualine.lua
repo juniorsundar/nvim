c62631686089b156a27b51738db5a9dc976a0204
Junior Sundar <juniorsundar@gmail.com>
2025-10-02 22:15:56 +0400

    Update lualine content

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -44,7 +44,7 @@ return {
             tabline = {},
             winbar = {},
             inactive_winbar = {},
-            extensions = { "oil", "quickfix", "mason", "man", "lazy" },
+            extensions = { "oil", "quickfix", "mason", "man", "lazy", "fugitive" },
         }
     end,
 }
