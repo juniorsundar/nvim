6a22d2e18e5bbd600210a2869f61bb05b7f37678
Junior Sundar <juniorsundar@gmail.com>
2025-10-05 14:29:41 +0400

    Format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
     event = "LspAttach",
     opts = {
         formatters_by_ft = {
+            c = { "clang-format" },
             python = { "ruff" },
             go = { "gofumpt" },
             rust = { "rustfmt" },
