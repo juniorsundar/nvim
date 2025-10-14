668021c8511161405df694296cb062f926caaa90
Junior Sundar <juniorsundar@gmail.com>
2025-10-14 21:28:28 +0400

    Oil needs to be setup manually

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,17 +1,18 @@
 return {
     "stevearc/oil.nvim",
-    opts = {
-        columns = {
-            "permissions",
-            "mtime",
-            "size",
-            "icon",
-        },
-        view_options = {
-            show_hidden = true,
-        },
-    },
-    keys = {
-        { "<leader>o", mode = { "n" }, "<cmd>Oil<cr>", desc = "Oil" }
-    }
+    lazy = false,
+    config = function()
+        require("oil").setup({
+            columns = {
+                "permissions",
+                "mtime",
+                "size",
+                "icon",
+            },
+            view_options = {
+                show_hidden = true,
+            },
+        })
+        vim.keymap.set("n", "<leader>o", "<cmd>Oil<cr>", { desc = "LSP", noremap = false, silent = true })
+    end
 }
