d7ccf9eae1c5e90095e4acbe8c287dc08ebc2a11
Junior Sundar <juniorsundar@gmail.com>
2025-10-15 23:48:03 +0400

    Oil borders

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -12,6 +12,9 @@ return {
             view_options = {
                 show_hidden = true,
             },
+            confirmation = {
+                border = "rounded"
+            }
         })
         vim.keymap.set("n", "<leader>o", "<cmd>Oil<cr>", { desc = "LSP", noremap = false, silent = true })
     end
