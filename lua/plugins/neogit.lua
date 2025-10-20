0d80caf710c4b8db4d099f5da11bc362747d4282
Junior Sundar <juniorsundar@gmail.com>
2025-10-20 23:34:54 +0400

    Reformatting

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,27 +1,27 @@
 return {
     "NeogitOrg/neogit",
     enabled = true,
-    keys = {
-        { "<leader>Gg", "<cmd>Neogit<cr>",           { desc = "Neogit" } },
-        { "<leader>G.", "<cmd>Neogit cwd=%:p:h<cr>", { desc = "Open to CWD" } }
-    },
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
-    opts = {
-        disable_hint = true,
-        signs = {
-            hunk = { "", "" },
-            item = { "", "" },
-            section = { "", "" },
-        },
-        graph_style = "unicode",
-        integrations = {
-            telescope = false,
-            diffview = true,
-            fzf_lua = false,
-            mini_pick = false,
-            snacks = true,
-        },
-    }
+    config = function()
+        require("neogit").setup({
+            disable_hint = true,
+            signs = {
+                hunk = { "", "" },
+                item = { "", "" },
+                section = { "", "" },
+            },
+            graph_style = "unicode",
+            integrations = {
+                telescope = false,
+                diffview = true,
+                fzf_lua = false,
+                mini_pick = false,
+                snacks = true,
+            },
+        })
+        vim.keymap.set("n", "<leader>Gg", "<cmd>Neogit<cr>", { desc = "Neogit" })
+        vim.keymap.set("n", "<leader>G.", "<cmd>Neogit cwd=%:p:h<cr>", { desc = "Open to CWD" })
+    end
 }
