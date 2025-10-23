a510f8fc586ae56eb27a5c33efcfb2dde47d677d
Junior Sundar <junior.sundar@tii.ae>
2025-10-23 19:56:39 +0400

    Simplifying lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,48 +1,195 @@
 return {
     "nvim-lualine/lualine.nvim",
     enabled = true,
-    opts = {
-        options = {
-            icons_enabled = true,
-            theme = "onedark",
-            component_separators = { left = " ", right = " " },
-            section_separators = { left = " ", right = " " },
-            disabled_filetypes = {
-                "NeogitStatus",
-                "snacks_dashboard",
-                "ministarter",
-                "fzf",
-                "neo-tree",
+    config = function()
+        local lualine = require('lualine')
+
+        local palette = require("theme.colors").dark
+        local colors = {
+            bg       = palette.bg_alt,
+            fg       = palette.fg,
+            yellow   = palette.yellow,
+            cyan     = palette.cyan,
+            darkblue = palette.dark_blue,
+            green    = palette.green,
+            orange   = palette.orange,
+            violet   = palette.violet,
+            magenta  = palette.magenta,
+            blue     = palette.blue,
+            red      = palette.red,
+        }
+
+        local conditions = {
+            buffer_not_empty = function()
+                return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+            end,
+            hide_in_width = function()
+                return vim.fn.winwidth(0) > 80
+            end,
+            check_git_workspace = function()
+                local filepath = vim.fn.expand('%:p:h')
+                local gitdir = vim.fn.finddir('.git', filepath .. ';')
+                return gitdir and #gitdir > 0 and #gitdir < #filepath
+            end,
+        }
+
+        -- Config
+        local config = {
+            options = {
+                icons_enabled = true,
+                disabled_filetypes = {
+                    "NeogitStatus",
+                    "snacks_dashboard",
+                    "ministarter",
+                    "fzf",
+                    "neo-tree",
+                },
+                component_separators = '',
+                section_separators = '',
+                theme = {
+                    normal = { c = { fg = colors.fg, bg = colors.bg } },
+                    inactive = { c = { fg = colors.fg, bg = colors.bg } },
+                },
             },
-            ignore_focus = {},
-            always_divide_middle = true,
-            always_show_tabline = true,
-            globalstatus = false,
-            refresh = {
-                statusline = 100,
-                tabline = 100,
-                winbar = 100,
+            sections = {
+                -- these are to remove the defaults
+                lualine_a = {},
+                lualine_b = {},
+                lualine_y = {},
+                lualine_z = {},
+                -- These will be filled later
+                lualine_c = {},
+                lualine_x = {},
             },
-        },
-        sections = {
-            lualine_a = { "mode" },
-            lualine_b = { "branch", "diagnostics" },
-            lualine_c = { "filename" },
-            lualine_x = { "filetype" },
-            lualine_y = { "location" },
-            lualine_z = { { "tabs", use_mode_colors = true } },
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = { "filename" },
-            lualine_x = { "location" },
-            lualine_y = {},
-            lualine_z = {},
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = { "oil", "quickfix", "mason", "man", "lazy", "fugitive" },
-    }
+            inactive_sections = {
+                -- these are to remove the defaults
+                lualine_a = {},
+                lualine_b = {},
+                lualine_y = {},
+                lualine_z = {},
+                lualine_c = {},
+                lualine_x = {},
+            },
+            extensions = { "oil", "quickfix", "mason", "man", "lazy", "fugitive" },
+
+        }
+
+        -- Inserts a component in lualine_c at left section
+        local function ins_left(component)
+            table.insert(config.sections.lualine_c, component)
+        end
+
+        -- Inserts a component in lualine_x at right section
+        local function ins_right(component)
+            table.insert(config.sections.lualine_x, component)
+        end
+
+        ins_left {
+            function()
+                return '▊'
+            end,
+            color = function()
+                -- auto change color according to neovims mode
+                local mode_color = {
+                    n = colors.red,
+                    i = colors.green,
+                    v = colors.blue,
+                    [''] = colors.blue,
+                    V = colors.blue,
+                    c = colors.magenta,
+                    no = colors.red,
+                    s = colors.orange,
+                    S = colors.orange,
+                    [''] = colors.orange,
+                    ic = colors.yellow,
+                    R = colors.violet,
+                    Rv = colors.violet,
+                    cv = colors.red,
+                    ce = colors.red,
+                    r = colors.cyan,
+                    rm = colors.cyan,
+                    ['r?'] = colors.cyan,
+                    ['!'] = colors.red,
+                    t = colors.red,
+                }
+                return { fg = mode_color[vim.fn.mode()] }
+            end,
+            padding = { left = 0, right = 1 },
+        }
+
+        ins_left {
+            'filename',
+            path = 1,
+            cond = conditions.buffer_not_empty,
+            color = { fg = colors.fg, gui = 'bold' },
+        }
+
+        ins_left {
+            'diagnostics',
+            sources = { 'nvim_diagnostic' },
+            symbols = { error = ' ', warn = ' ', info = ' ' },
+            diagnostics_color = {
+                error = { fg = colors.red },
+                warn = { fg = colors.yellow },
+                info = { fg = colors.cyan },
+            },
+        }
+
+        ins_left {
+            function()
+                return '%='
+            end,
+        }
+
+        ins_right {
+            'branch',
+            icon = '',
+            color = { fg = colors.violet, gui = 'bold' },
+        }
+
+        ins_right {
+            'diff',
+            symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
+            diff_color = {
+                added = { fg = colors.green },
+                modified = { fg = colors.orange },
+                removed = { fg = colors.red },
+            },
+            cond = conditions.hide_in_width,
+        }
+
+        ins_right {
+            function()
+                return '▊'
+            end,
+            color = function()
+                local mode_color = {
+                    n = colors.red,
+                    i = colors.green,
+                    v = colors.blue,
+                    [''] = colors.blue,
+                    V = colors.blue,
+                    c = colors.magenta,
+                    no = colors.red,
+                    s = colors.orange,
+                    S = colors.orange,
+                    [''] = colors.orange,
+                    ic = colors.yellow,
+                    R = colors.violet,
+                    Rv = colors.violet,
+                    cv = colors.red,
+                    ce = colors.red,
+                    r = colors.cyan,
+                    rm = colors.cyan,
+                    ['r?'] = colors.cyan,
+                    ['!'] = colors.red,
+                    t = colors.red,
+                }
+                return { fg = mode_color[vim.fn.mode()] }
+            end,
+            padding = { left = 1 },
+        }
+
+        lualine.setup(config)
+    end
 }
