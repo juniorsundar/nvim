bd89075d5524a3654ede16135bd40f967339121d
Junior Sundar <juniorsundar@gmail.com>
2025-11-01 16:22:31 +0400

    Oil is not lazyloaded

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,24 +1,19 @@
-MiniDeps.later(function()
+MiniDeps.now(function()
     MiniDeps.add { source = "stevearc/oil.nvim" }
+    require("oil").setup {
+        columns = {
+            "permissions",
+            "mtime",
+            "size",
+            "icon",
+        },
+        view_options = {
+            show_hidden = true,
+        },
+        confirmation = {
+            border = "rounded",
+        },
+    }
 
-    vim.keymap.set("n", "<leader>o", function()
-        if not package.loaded["oil"] then
-            require("oil").setup {
-                columns = {
-                    "permissions",
-                    "mtime",
-                    "size",
-                    "icon",
-                },
-                view_options = {
-                    show_hidden = true,
-                },
-                confirmation = {
-                    border = "rounded",
-                },
-            }
-        end
-
-        vim.cmd "Oil"
-    end, { desc = "Oil", noremap = false, silent = true })
+    vim.keymap.set("n", "<leader>o", function() vim.cmd "Oil" end, { desc = "Oil", noremap = false, silent = true })
 end)
