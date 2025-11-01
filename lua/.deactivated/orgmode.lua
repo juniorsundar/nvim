8203e6cafe458a43b8d113f1105fe9ebe21a6eb8
Junior Sundar <juniorsundar@gmail.com>
2025-11-01 23:40:07 +0400

    Added persistence.nvim

---
+++ lua/.deactivated/orgmode.lua
@@ -0,0 +1,17 @@
+MiniDeps.later(function()
+    MiniDeps.add({ source = 'nvim-orgmode/orgmode', depends = { "chipsenkbeil/org-roam.nvim" } })
+    -- Setup orgmode
+    require('orgmode').setup({
+        mappings = {
+            prefix = "<LocalLeader>O"
+        },
+        org_agenda_files = '~/Dropbox/org/**/*',
+        org_default_notes_file = '~/Dropbox/org/refile.org',
+        org_todo_keywords = {'TODO(t)', 'DOING(d)', '|', 'DONE(D)', 'CANCELLED(C)'},
+    })
+
+    require("org-roam").setup({
+        bindings = { prefix = "<LocalLeader>OR" },
+        directory = "~/Dropbox/org/pages/",
+    })
+end)
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -53,7 +53,7 @@ MiniDeps.now(function()
                 height = 1.0,
                 border = "top",
             },
-            { win = "list", height = 0.25, border = "top" },
+            { win = "list",    height = 0.25,       border = "top" },
         },
     }
     require("snacks.picker.config.layouts").sidebar.layout.position = "right"
@@ -104,21 +104,36 @@ MiniDeps.now(function()
                             Snacks.dashboard.pick("files", { cwd = vim.fn.stdpath "config" })
                         end,
                     },
-                    {
-                        icon = "󰒲 ",
-                        key = "L",
-                        desc = "Lazy",
-                        action = ":Lazy",
-                        enabled = package.loaded.lazy ~= nil,
-                    },
+                    -- { icon = " ", key = "s", desc = "Restore Session", section = "session" },
                     { icon = " ", key = "q", desc = "Quit", action = ":qa" },
                 },
             },
             sections = {
                 { section = "header" },
-                { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-                { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-                { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+                -- {
+                --     pane = 2,
+                --     section = "terminal",
+                --     cmd = "colorscript -e square",
+                --     height = 5,
+                --     padding = 1,
+                -- },
+                { section = "keys", gap = 1, padding = 1 },
+                { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+                { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+                {
+                    pane = 2,
+                    icon = " ",
+                    title = "Git Status",
+                    section = "terminal",
+                    enabled = function()
+                        return Snacks.git.get_root() ~= nil
+                    end,
+                    cmd = "git status --short --branch --renames",
+                    height = 5,
+                    padding = 1,
+                    ttl = 5 * 60,
+                    indent = 3,
+                },
                 -- { section = "startup" },
             },
         },
