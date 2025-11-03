e7007d6507f282226f8271a37034b9440f8ce38a
Junior Sundar <junior.sundar@tii.ae>
2025-11-03 13:38:57 +0400

    Auto-add all plugins in plugin directory

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,21 +1,18 @@
-require("plugins.catppuccin")
-require("plugins.nvim-web-devicons")
-require("plugins.which-key")
-require("plugins.snacks")
-require("plugins.lualine")
-require("plugins.oil")
-require("plugins.blink")
-require("plugins.treesitter")
-require("plugins.nvim-surround")
-require("plugins.nvim-lint")
-require("plugins.mini")
-require("plugins.mason")
-require("plugins.conform")
-require("plugins.lazydev")
-require("plugins.diffview")
-require("plugins.gitsigns")
-require("plugins.vim-fugitive")
-require("plugins.flash")
-require("plugins.quicker")
-require("plugins.render-markdown")
-require("plugins.persistence")
+local config_path = vim.fn.stdpath('config')
+local plugins_dir = config_path .. '/lua/plugins'
+
+for file_name, file_type in vim.fs.dir(plugins_dir) do
+    if file_type == 'file' then
+        local module_name = string.match(file_name, "^(.-)%.lua$")
+        if module_name and module_name ~= 'init' then
+            local require_path = 'plugins.' .. module_name
+            local ok, err = pcall(require, require_path)
+            if not ok then
+                vim.notify(
+                    "Error loading plugin config: " .. require_path .. "\n" .. err,
+                    vim.log.levels.ERROR
+                )
+            end
+        end
+    end
+end
