13d70e7ba7e7cf1c53bcc7d6aa9e93cc10bf34d0
Junior Sundar <juniorsundar@gmail.com>
2025-11-05 20:52:36 +0400

    Removed nvim lint

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,26 +1,24 @@
-MiniDeps.later(function()
-	MiniDeps.add({ source = "nvim-mini/mini.ai" })
-	MiniDeps.add({ source = "nvim-mini/mini.move" })
-	vim.api.nvim_create_autocmd("BufEnter", {
-		pattern = "*",
-		once = true,
-		callback = function()
-			require("mini.ai").setup({
-				n_lines = 500,
-			})
+MiniDeps.add({ source = "nvim-mini/mini.ai" })
+MiniDeps.add({ source = "nvim-mini/mini.move" })
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*",
+    once = true,
+    callback = function()
+        require("mini.ai").setup({
+            n_lines = 500,
+        })
 
-			require("mini.move").setup({
-				mappings = {
-					left = "<S-left>",
-					right = "<S-right>",
-					down = "<S-down>",
-					up = "<S-up>",
-					line_left = "<S-left>",
-					line_right = "<S-right>",
-					line_down = "<S-down>",
-					line_up = "<S-up>",
-				},
-			})
-		end,
-	})
-end)
+        require("mini.move").setup({
+            mappings = {
+                left = "<S-left>",
+                right = "<S-right>",
+                down = "<S-down>",
+                up = "<S-up>",
+                line_left = "<S-left>",
+                line_right = "<S-right>",
+                line_down = "<S-down>",
+                line_up = "<S-up>",
+            },
+        })
+    end,
+})
