3e9a0f937398f3156c573c378eb150ef75391120
Junior Sundar <juniorsundar@gmail.com>
2025-11-07 22:46:36 +0400

    Fugitive to Neogit

---
+++ lua/plugins/11_neogit.lua
@@ -0,0 +1,19 @@
+MiniDeps.add { source = "NeogitOrg/neogit", depends = { "nvim-lua/plenary.nvim" } }
+require("neogit").setup {
+    disable_hint = true,
+    signs = {
+        hunk = { "", "" },
+        item = { "", "" },
+        section = { "", "" },
+    },
+    graph_style = "unicode",
+    integrations = {
+        telescope = nil,
+        diffview = true,
+        fzf_lua = nil,
+        mini_pick = nil,
+        snacks = true,
+    },
+}
+
+vim.keymap.set("n", "<leader>Gg", "<cmd>:Neogit cwd=%:p:h<cr>", { desc = "Neogit", noremap = false, silent = true })
--- lua/plugins/11_vim-fugitive.lua
+++
@@ -1,3 +0,0 @@
-MiniDeps.add { source = "tpope/vim-fugitive" }
-
-vim.keymap.set("n", "<leader>Gg", "<cmd>tab Git<cr>", { desc = "Fugitive", noremap = false, silent = true })
