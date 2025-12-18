d12ad9806d8dec404d0680a3661e3dc681807a16
Junior Sundar <juniorsundar@gmail.com>
2025-12-18 16:49:19 +0400

    Cleaning up some more stuff

--- lua/.deactivated/neogit.lua
+++ lua/.deactivated/neogit.lua
@@ -1,27 +1,19 @@
-return {
-    "NeogitOrg/neogit",
-    enabled = true,
-    dependencies = {
-        "nvim-lua/plenary.nvim",
+MiniDeps.add { source = "NeogitOrg/neogit", depends = { "nvim-lua/plenary.nvim" } }
+require("neogit").setup {
+    disable_hint = true,
+    signs = {
+        hunk = { "", "" },
+        item = { "", "" },
+        section = { "", "" },
     },
-    config = function()
-        require("neogit").setup {
-            disable_hint = true,
-            signs = {
-                hunk = { "", "" },
-                item = { "", "" },
-                section = { "", "" },
-            },
-            graph_style = "unicode",
-            integrations = {
-                telescope = nil,
-                diffview = true,
-                fzf_lua = nil,
-                mini_pick = nil,
-                snacks = true,
-            },
-        }
-        vim.keymap.set("n", "<leader>Gg", "<cmd>Neogit<cr>", { desc = "Neogit" })
-        vim.keymap.set("n", "<leader>G.", "<cmd>Neogit cwd=%:p:h<cr>", { desc = "Open to CWD" })
-    end,
+    graph_style = "unicode",
+    integrations = {
+        telescope = nil,
+        diffview = true,
+        fzf_lua = nil,
+        mini_pick = nil,
+        snacks = true,
+    },
 }
+vim.keymap.set("n", "<leader>Gg", "<cmd>Neogit<cr>", { desc = "Neogit" })
+vim.keymap.set("n", "<leader>G.", "<cmd>Neogit cwd=%:p:h<cr>", { desc = "Open to CWD" })
