2320b88d6669384ab723dc21cf02be6f49cc0db5
Junior Sundar <juniorsundar@gmail.com>
2026-01-16 14:25:10 +0400

    Revert to doom-one

--- after/lsp/clangd.lua
+++ after/lsp/clangd.lua
@@ -3,6 +3,6 @@ local capabilities = require "config.lsp.serve_capabilities"
 return {
     cmd = { "clangd" },
     filetypes = { "cpp", "hpp", "h", "c", "cuda" },
-    root_markers = { "compile_commands.json", ".clangd", ".git" },
+    root_markers = { "compile_commands.json", ".clangd" },
     capabilities = capabilities,
 }
