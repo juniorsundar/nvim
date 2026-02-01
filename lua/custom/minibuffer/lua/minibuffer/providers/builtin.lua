9199808f85e86188289fbbbc44118acb0f03705e
Junior Sundar <juniorsundar@gmail.com>
2026-02-01 23:25:44 +0400

    Cleaning

--- lua/custom/minibuffer/lua/minibuffer/providers/builtin.lua
+++ lua/custom/minibuffer/lua/minibuffer/providers/builtin.lua
@@ -1,5 +1,6 @@
 local M = {}
 local minibuffer = require "minibuffer"
+local util = require "minibuffer.util"
 
 function M.commands()
     minibuffer.pick(function(input)
@@ -10,6 +11,32 @@ function M.commands()
     end, function(input_text)
         vim.cmd(input_text)
     end, { prompt = "M-x > " })
+end
+
+function M.buffers()
+    local bufs = vim.api.nvim_list_bufs()
+    local items = {}
+
+    for _, bufnr in ipairs(bufs) do
+        if vim.api.nvim_buf_is_loaded(bufnr) and vim.bo[bufnr].buflisted then
+            local name = vim.api.nvim_buf_get_name(bufnr)
+            if name ~= "" then
+                local relative_path = util.get_relative_path(name)
+                local entry = string.format("%d: %s", bufnr, relative_path)
+                table.insert(items, entry)
+            end
+        end
+    end
+
+    minibuffer.pick(items, function(selection)
+        util.jump_to_location(selection)
+    end, {
+        prompt = "Buffers > ",
+        keymaps = {
+            ["<Tab>"] = "toggle_mark",
+            ["<CR>"] = "select_entry",
+        },
+    })
 end
 
 return M
--- lua/plugins/104_minibuffer.lua
+++ lua/plugins/104_minibuffer.lua
@@ -6,5 +6,6 @@ vim.keymap.set("n", "<A-x>", builtin.commands, { desc = "Command Palette" })
 
 vim.keymap.set("n", "<C-x>r", lsp.references, { desc = "LSP References" })
 vim.keymap.set("n", "<C-x>d", lsp.definitions, { desc = "LSP Definitions" })
+vim.keymap.set("n", "<C-x>b", builtin.buffers, { desc = "Buffers" })
 
 return M
