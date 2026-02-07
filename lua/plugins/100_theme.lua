6214be7001f440d0a15351d26af306d344752109
Junior Sundar <juniorsundar@gmail.com>
2026-02-08 00:34:15 +0400

    Cleaning my directory

---
+++ lua/plugins/100_theme.lua
@@ -0,0 +1,36 @@
+MiniDeps.add { source = "juniorsundar/doom-one.nvim" }
+-- Add color to cursor
+vim.g.doom_one_cursor_coloring = false
+-- Set :terminal colors
+vim.g.doom_one_terminal_colors = false
+-- Enable italic comments
+vim.g.doom_one_italic_comments = true
+-- Enable TS support
+vim.g.doom_one_enable_treesitter = true
+-- Color whole diagnostic text or only underline
+vim.g.doom_one_diagnostics_text_color = false
+-- Enable transparent background
+vim.g.doom_one_transparent_background = false
+
+-- Pumblend transparency
+vim.g.doom_one_pumblend_enable = false
+vim.g.doom_one_pumblend_transparency = 0
+
+-- Plugins integration
+vim.g.doom_one_plugin_neorg = true
+vim.g.doom_one_plugin_barbar = false
+vim.g.doom_one_plugin_telescope = false
+vim.g.doom_one_plugin_neogit = true
+vim.g.doom_one_plugin_nvim_tree = true
+vim.g.doom_one_plugin_dashboard = true
+vim.g.doom_one_plugin_startify = true
+vim.g.doom_one_plugin_whichkey = true
+vim.g.doom_one_plugin_snacks = true
+vim.g.doom_one_plugin_blink = true
+vim.g.doom_one_plugin_nvim_cmp = true
+vim.g.doom_one_plugin_indent_blankline = true
+vim.g.doom_one_plugin_vim_illuminate = true
+vim.g.doom_one_plugin_lspsaga = false
+vim.g.doom_one_plugin_multicursor = true
+
+vim.cmd "colorscheme doom-one"
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,15 +1,22 @@
 local config_path = vim.fn.stdpath "config"
 local plugins_dir = config_path .. "/lua/plugins"
 
+local files = {}
 for file_name, file_type in vim.fs.dir(plugins_dir) do
     if file_type == "file" then
         local module_name = string.match(file_name, "^(.-)%.lua$")
         if module_name and module_name ~= "init" then
-            local require_path = "plugins." .. module_name
-            local ok, err = pcall(require, require_path)
-            if not ok then
-                vim.notify("Error loading plugin config: " .. require_path .. "\n" .. err, vim.log.levels.ERROR)
-            end
+            table.insert(files, module_name)
         end
+    end
+end
+
+table.sort(files)
+
+for _, module_name in ipairs(files) do
+    local require_path = "plugins." .. module_name
+    local ok, err = pcall(require, require_path)
+    if not ok then
+        vim.notify("Error loading plugin config: " .. require_path .. "\n" .. err, vim.log.levels.ERROR)
     end
 end
