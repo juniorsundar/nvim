db77b7eaa6f784774f5ec88b421e58bb7210e271
Junior Sundar <juniorsundar@gmail.com>
2026-02-09 23:31:26 +0400

    Update refer

--- lua/plugins/18_minibuffer.lua
+++ lua/plugins/18_minibuffer.lua
@@ -1,4 +1,4 @@
-local M = require "refer"
+local refer = require "refer"
 local builtin = require "refer.providers.builtin"
 local lsp = require "refer.providers.lsp"
 local files = require "refer.providers.files"
@@ -11,4 +11,4 @@ vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Buffers" })
 vim.keymap.set("n", "<leader>Ff", files.files, { desc = "Files" })
 vim.keymap.set("n", "<leader>Ft", files.live_grep, { desc = "Text" })
 
-return M
+return refer
