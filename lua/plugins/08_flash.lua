dd572e47433858617c65bace12a87a6f667669ee
Junior Sundar <juniorsundar@gmail.com>
2026-02-27 11:27:17 +0400

    Enable function with vscode emulation

--- lua/plugins/08_flash.lua
+++ lua/plugins/08_flash.lua
@@ -1,10 +1,22 @@
 MiniDeps.add { source = "folke/flash.nvim" }
 
 require("flash").setup()
-vim.keymap.set({ "n", "x", "o" }, "<C-s>", function()
+local is_vscode = vim.g.vscode
+local jump_key = is_vscode and "<leader>s" or "<C-s>"
+local treesitter_key = is_vscode and "<leader>S" or "<C-M-s>"
+local incremental_key = is_vscode and "<leader>v" or "<c-space>"
+local incremental_actions = is_vscode and {
+    ["n"] = "next",
+    ["p"] = "prev",
+} or {
+    ["<c-space>"] = "next",
+    ["<BS>"] = "prev",
+}
+
+vim.keymap.set({ "n", "x", "o" }, jump_key, function()
     require("flash").jump()
 end, { desc = "Flash", noremap = false, silent = true })
-vim.keymap.set({ "n", "x", "o" }, "<C-M-s>", function()
+vim.keymap.set({ "n", "x", "o" }, treesitter_key, function()
     require("flash").treesitter()
 end, { desc = "Flash Treesitter", noremap = false, silent = true })
 vim.keymap.set("o", "gr", function()
@@ -13,11 +25,8 @@ end, { desc = "Remote Flash", noremap = false, silent = true })
 vim.keymap.set({ "o", "x" }, "gR", function()
     require("flash").treesitter_search()
 end, { desc = "Treesitter Search", noremap = false, silent = true })
-vim.keymap.set({ "n", "x", "o" }, "<c-space>", function()
+vim.keymap.set({ "n", "x", "o" }, incremental_key, function()
     require("flash").treesitter {
-        actions = {
-            ["<c-space>"] = "next",
-            ["<BS>"] = "prev",
-        },
+        actions = incremental_actions,
     }
 end, { desc = "Treesitter incremental selection", noremap = false, silent = true })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,12 +1,20 @@
 local config_path = vim.fn.stdpath "config"
 local plugins_dir = config_path .. "/lua/plugins"
-
 local files = {}
-for file_name, file_type in vim.fs.dir(plugins_dir) do
-    if file_type == "file" then
-        local module_name = string.match(file_name, "^(.-)%.lua$")
-        if module_name and module_name ~= "init" then
-            table.insert(files, module_name)
+
+if vim.g.vscode then
+    files = {
+        "06_nvim-surround",
+        "08_flash",
+        "09_mini",
+    }
+else
+    for file_name, file_type in vim.fs.dir(plugins_dir) do
+        if file_type == "file" then
+            local module_name = string.match(file_name, "^(.-)%.lua%")
+            if module_name and module_name ~= "init" then
+                table.insert(files, module_name)
+            end
         end
     end
 end
