de84b777c6b5a8b03260dce9bdb58221c0b27037
Junior Sundar <juniorsundar@gmail.com>
2026-03-09 22:42:29 +0400

    tried agentic.nvim

--- after/lsp/c3-lsp.lua
+++ after/lsp/c3-lsp.lua
@@ -1,7 +1,7 @@
 local capabilities = require "config.lsp.serve_capabilities"
 
 return {
-    cmd = { "c3lsp" },
+    cmd = { "c3-lsp" },
     filetypes = { "c3", "c3i" },
     root_markers = { "project.json", "manifest.json", ".git" },
     capabilities = capabilities,
---
+++ lua/.deactivated/agentic.lua
@@ -0,0 +1,31 @@
+MiniDeps.later(function()
+    MiniDeps.add { source = "carlos-algms/agentic.nvim" }
+
+    require("agentic").setup {
+        provider = "opencode-acp",
+    }
+
+    vim.keymap.set({ "n", "v", "i" }, "<C-\\>", function()
+        require("agentic").toggle()
+    end, { desc = "Toggle Agentic Chat" })
+
+    vim.keymap.set({ "n", "v" }, "<C-'>", function()
+        require("agentic").add_selection_or_file_to_context()
+    end, { desc = "Add file or selection to Agentic to Context" })
+
+    vim.keymap.set({ "n", "v", "i" }, "<C-,>", function()
+        require("agentic").new_session()
+    end, { desc = "New Agentic Session" })
+
+    vim.keymap.set({ "n", "v", "i" }, "<A-i>r", function()
+        require("agentic").restore_session()
+    end, { desc = "Agentic Restore session", silent = true })
+
+    vim.keymap.set({ "n" }, "<leader>ad", function()
+        require("agentic").add_current_line_diagnostics()
+    end, { desc = "Add current line diagnostic to Agentic" })
+
+    vim.keymap.set({ "n" }, "<leader>aD", function()
+        require("agentic").add_buffer_diagnostics()
+    end, { desc = "Add all buffer diagnostics to Agentic" })
+end)
