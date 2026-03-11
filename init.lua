24cdee0dd86db43e33c3aaadf3b2b992c4474a2a
Junior Sundar <juniorsundar@gmail.com>
2026-03-11 21:01:06 +0400

    Toggle line number

--- init.lua
+++ init.lua
@@ -12,8 +12,6 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- UI config
-vim.opt.number = true
-vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
