f7a48e66f0afa4282fb9574dc983dfd1934173a3
Junior Sundar <juniorsundar@gmail.com>
2026-03-13 16:38:13 +0400

    Add nvim-tree

---
+++ lua/plugins/07_nvim-tree.lua
@@ -0,0 +1,17 @@
+MiniDeps.now(function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    MiniDeps.add { source = "nvim-tree/nvim-tree.lua" }
+
+    require("nvim-tree").setup {
+        view = {
+            side = "right",
+        },
+        update_focused_file = {
+            enable = true,
+            update_root = false,
+        },
+    }
+    vim.api.nvim_set_hl(0, "NvimTreeCursorLine", { link = "Visual" })
+end)
