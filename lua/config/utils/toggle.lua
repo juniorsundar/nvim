5d80b5db9ac365a02f352797d3d36116921058b9
Junior Sundar <juniorsundar@gmail.com>
2026-03-18 22:35:13 +0400

    Togglers

---
+++ lua/config/utils/toggle.lua
@@ -0,0 +1,14 @@
+vim.keymap.set("n", "<localleader>Tn", "", { desc = "Toggle" })
+
+vim.keymap.set("n", "<localleader>Tn", function()
+    local current_win = vim.api.nvim_get_current_win()
+    local number = vim.api.nvim_get_option_value("number", { win = current_win })
+    local relativenumber = vim.api.nvim_get_option_value("relativenumber", { win = current_win })
+
+    vim.api.nvim_set_option_value("number", not number, { win = current_win })
+    vim.api.nvim_set_option_value("relativenumber", not relativenumber, { win = current_win })
+end, { desc = "Line Numbers" })
+
+vim.keymap.set("n", "<localleader>Ti", function()
+    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+end, { desc = "LSP Inlay Hints" })
