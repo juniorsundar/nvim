b093bdafc754958b30aa296bebb7843f8a8d1504
Junior Sundar <juniorsundar@gmail.com>
2026-03-30 23:29:34 +0400

    Suffixed window cleared

--- lua/custom/micro.nvim/lua/micro/split_suffix.lua
+++ lua/custom/micro.nvim/lua/micro/split_suffix.lua
@@ -1,10 +1,13 @@
 local M = {}
 local ns_id = vim.api.nvim_create_namespace "split_suffix_beacon"
 
+M.queue = {}
+
 local default = {
     modifiers = {
-        ["vsplit"] = "<C-v>",
-        ["split"] = "<C-h>",
+        ["vsplit"] = "<C-w><C-v>",
+        ["split"] = "<C-w><C-h>",
+        ["clear"] = "<C-w><C-x>",
     },
 }
 
@@ -13,6 +16,7 @@ local function split_suffix(command)
     local line_dest = vim.api.nvim_win_get_cursor(0)[1] - 1
 
     vim.cmd(command)
+    table.insert(M.queue, vim.api.nvim_get_current_win())
 
     vim.cmd "wincmd p"
     vim.cmd [[execute "normal! \<C-o>"]]
@@ -28,12 +32,21 @@ local function split_suffix(command)
 
     vim.defer_fn(function()
         if vim.api.nvim_buf_is_valid(buf_dest) then
-            vim.api.nvim_buf_clear_namespace(buf_dest, ns_id, line_dest, line_dest + 1)
+            vim.api.nvim_buf_clear_namespace(buf_dest, ns_id, 0, -1)
         end
         if vim.api.nvim_buf_is_valid(buf_orig) then
-            vim.api.nvim_buf_clear_namespace(buf_orig, ns_id, line_orig, line_orig + 1)
+            vim.api.nvim_buf_clear_namespace(buf_orig, ns_id, 0, -1)
         end
     end, 1000)
+end
+
+local function clear_opened_windows()
+    for _, v in ipairs(M.queue) do
+        if vim.api.nvim_win_is_valid(v) then
+            vim.api.nvim_win_close(v, true)
+        end
+    end
+    M.queue = {}
 end
 
 function M.setup(opts)
@@ -41,10 +54,13 @@ function M.setup(opts)
 
     vim.keymap.set("n", default.modifiers.vsplit, function()
         split_suffix "vsplit"
-    end)
+    end, { desc = "Vertical Split Suffix" })
     vim.keymap.set("n", default.modifiers.split, function()
         split_suffix "split"
-    end)
+    end, { desc = "Horizontal Split Suffix" })
+    vim.keymap.set("n", default.modifiers.clear, function()
+        clear_opened_windows()
+    end, { desc = "Close Suffixed Windows" })
 end
 
 return M
