894171ffbb68ec115a7f29aad3dc3142e04e0e7d
Junior Sundar <juniorsundar@gmail.com>
2026-03-31 21:39:45 +0400

    Which key setting

--- lua/plugins/04_which-key.lua
+++ lua/plugins/04_which-key.lua
@@ -6,7 +6,7 @@ MiniDeps.now(function()
     require("which-key.colors").Normal = "NormalFloat"
 
     require("which-key").setup {
-        preset = "helix",
+        preset = "classic",
         delay = function(ctx)
             return ctx.plugin and 0 or 1000
         end,
@@ -14,12 +14,11 @@ MiniDeps.now(function()
             rules = false,
         },
         win = {
-            row = -2,
+            border = { "─", "─", "─", "", "", "", "", "" },
             padding = { 1, 2 },
             title = true,
             title_pos = "center",
             zindex = 1000,
-            border = "solid",
         },
         layout = {
             width = { min = 20 },
@@ -27,4 +26,10 @@ MiniDeps.now(function()
             align = "center",
         },
     }
+
+    local normal_bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
+    local border_fg = vim.api.nvim_get_hl(0, { name = "FloatBorder" }).fg
+
+    vim.api.nvim_set_hl(0, "WhichKeyNormal", { bg = normal_bg })
+    vim.api.nvim_set_hl(0, "WhichKeyBorder", { fg = border_fg, bg = normal_bg })
 end)
