70b8e62d2c5f9cee72a464f1cf13ded95427140a
Junior Sundar <juniorsundar@gmail.com>
2026-04-27 00:59:08 +0400

    Change deactivated themes for micro pack

--- lua/.deactivated/themes/tokyonight.lua
+++ lua/.deactivated/themes/tokyonight.lua
@@ -1,11 +1,9 @@
-MiniDeps.now(function()
-    MiniDeps.add { source = "folke/tokyonight.nvim", name = "catppuccin" }
-    require("tokyonight").setup {
-        cache = true,
-        plugins = {
-            all = true,
-        },
-    }
+require("micro.pack").add { src = "gh:folke/tokyonight.nvim", name = "tokyonight" }
+require("tokyonight").setup {
+    cache = true,
+    plugins = {
+        all = true,
+    },
+}
 
-    vim.cmd [[colorscheme tokyonight]]
-end)
+vim.cmd [[colorscheme tokyonight]]
