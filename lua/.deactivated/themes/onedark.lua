366d35ac96fd3c299726e0b300267bd7f57fb61b
Junior Sundar <juniorsundar@gmail.com>
2026-04-27 00:59:08 +0400

    Change deactivated themes for micro pack

--- lua/.deactivated/themes/onedark.lua
+++ lua/.deactivated/themes/onedark.lua
@@ -1,14 +1,12 @@
-MiniDeps.now(function()
-    MiniDeps.add { source = "navarasu/onedark.nvim" }
+require("micro.pack").add { src = "gh:navarasu/onedark.nvim" }
 
-    require("onedark").setup {
-        style = "dark",
-        highlights = {
-            ["DiagnosticHint"] = { fg = "$green" },
-            ["DiagnosticUnderlineHint"] = { sp = "$green" },
-            ["CursorLine"] = { bg = "$bg0" },
-            ["Folded"] = { bg = "$bg0" },
-        },
-    }
-    require("onedark").load()
-end)
+require("onedark").setup {
+    style = "dark",
+    highlights = {
+        ["DiagnosticHint"] = { fg = "$green" },
+        ["DiagnosticUnderlineHint"] = { sp = "$green" },
+        ["CursorLine"] = { bg = "$bg0" },
+        ["Folded"] = { bg = "$bg0" },
+    },
+}
+require("onedark").load()
