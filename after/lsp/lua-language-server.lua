9a8bd52232fc6fc0b1186c6cbd6ddc511a35e6ef
Junior Sundar <juniorsundar@gmail.com>
2026-09-09 01:22:56 +0400

    Fix lua-ls startup

--- after/lsp/lua-language-server.lua
+++ after/lsp/lua-language-server.lua
@@ -1,22 +1,26 @@
 local capabilities = require "config.lsp.serve_capabilities"
 
+local root_markers1 = {
+    ".emmyrc.json",
+    ".luarc.json",
+    ".luarc.jsonc",
+}
+local root_markers2 = {
+    ".luacheckrc",
+    ".stylua.toml",
+    "stylua.toml",
+    "selene.toml",
+    "selene.yml",
+}
+
 return {
     cmd = { "lua-language-server" },
     filetypes = { "lua" },
+    root_markers = vim.fn.has "nvim-0.11.3" == 1 and { root_markers1, root_markers2, { ".git" } }
+        or vim.list_extend(vim.list_extend(root_markers1, root_markers2), { ".git" }),
     capabilities = capabilities,
     settings = {
         Lua = {
-            runtime = { version = "LuaJIT" },
-            workspace = {
-                checkThirdParty = false,
-                library = {
-                    "${3rd}/luv/library",
-                    unpack(vim.api.nvim_get_runtime_file("", true)),
-                },
-            },
-            completion = {
-                callSnippet = "Replace",
-            },
             hint = {
                 enable = true,
             },
