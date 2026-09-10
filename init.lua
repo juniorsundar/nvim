089ad4c93afb7cbf136eb47b7bc53be68adfee5d
Junior Sundar <juniorsundar@gmail.com>
2026-09-10 13:23:54 +0400

    Remove padding in terminal buffers

--- init.lua
+++ init.lua
@@ -141,3 +141,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
         vim.cmd "normal! G$"
     end,
 })
+
+vim.api.nvim_create_autocmd("TermOpen", {
+    callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.signcolumn = "no"
+        vim.opt_local.relativenumber = false
+        vim.opt_local.statuscolumn = ""
+    end,
+})
